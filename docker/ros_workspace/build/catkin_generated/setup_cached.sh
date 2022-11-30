e2c85e67757b6f7a6e29487098bb24122d7f7016
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-11-30 19:51:36 +0900

    2020e

--- docker/ros_workspace/build/catkin_generated/setup_cached.sh
+++ docker/ros_workspace/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/hayashide/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/hayashide/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/hayashide/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/hayashide/catkin_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/hayashide/catkin_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/hayashide/catkin_ws/src:$ROS_PACKAGE_PATH"
--- sotsuron_experiment/scripts/csv_plotter.py
+++ sotsuron_experiment/scripts/csv_plotter.py
@@ -3,6 +3,37 @@ import matplotlib.pyplot as plt
 
 data=np.loadtxt("results.csv",delimiter=",")
 print(data)
+
+col=1
+x_plot=[data[0][col]]
+for i, row in enumerate(data[1:-1]):
+    i+=1
+    if abs(row[col]-data[i-1][col])>2000 or abs(row[col]-data[i+1][col])>2000:
+        x_plot.append(np.nan)
+    else:
+        x_plot.append(row[col])
+x_plot.append(data[-1][col])
+
+col=2
+y_plot=[data[0][col]]
+for i, row in enumerate(data[1:-1]):
+    i+=1
+    if abs(row[col]-data[i-1][col])>2000 or abs(row[col]-data[i+1][col])>2000:
+        y_plot.append(np.nan)
+    else:
+        y_plot.append(row[col])
+y_plot.append(data[-1][col])
+
+col=3
+z_plot=[data[0][col]]
+for i, row in enumerate(data[1:-1]):
+    i+=1
+    if abs(row[col]-data[i-1][col])>2000 or abs(row[col]-data[i+1][col])>2000:
+        z_plot.append(np.nan)
+    else:
+        z_plot.append(row[col])
+z_plot.append(data[-1][col])
+
 col=-1
 vel_plot=[data[0][col]]
 for i, row in enumerate(data[1:-1]):
@@ -19,6 +50,10 @@ for i,row in enumerate(vel_plot[window:]):
     i+=window
     ave=np.average(vel_plot[i-window:i])
     vel_ave.append(ave)
-plt.plot(data[:,0],vel_plot)
-plt.plot(data[:,0],vel_ave)
+plt.scatter(data[:,0],x_plot,label="x")
+plt.scatter(data[:,0],y_plot,label="y")
+plt.scatter(data[:,0],z_plot,label="z")
+# plt.plot(data[:,0],vel_plot,label="vel_raw")
+# plt.plot(data[:,0],vel_ave,label="vel_ave")
+plt.legend()
 plt.show()
