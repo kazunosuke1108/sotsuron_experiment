798d3915a6d61d44511daed4c133dc1aa757d52c
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-12-08 22:54:19 +0900

    end of today

--- sotsuron_experiment/scripts/mat_reader.py
+++ sotsuron_experiment/scripts/mat_reader.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matlab.engine
 eng = matlab.engine.start_matlab()
-content = eng.load("/home/hayashide/kazu_ws/sotsuron_simulator/matlab_ws/1201/results/1208_07304v2/221208_185015_1.5m20m2Hz/221208_185015_test.mat", nargout=1)
+content = eng.load("/home/hayashide/kazu_ws/sotsuron_simulator/matlab_ws/1201/results/1208_07hallv3/221208_191150_2Hz/221208_191150_test.mat", nargout=1)
 # print(np.array(content['z']).shape)
 z=np.array(content['z'])
 t=np.array(content['t'])
-np.savetxt("/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/scripts/monitor/z20.csv",z,delimiter=",")
-np.savetxt("/home/hayashide/ytlab_ros_ws/ytlab_hsr/ytlab_hsr_modules/datas/z20.csv",z,delimiter=",")
-np.savetxt("/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/scripts/monitor/t20.csv",t,delimiter=",")
-np.savetxt("/home/hayashide/ytlab_ros_ws/ytlab_hsr/ytlab_hsr_modules/datas/t20.csv",t,delimiter=",")
+np.savetxt("/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/scripts/monitor/z2hzH.csv",z,delimiter=",")
+np.savetxt("/home/hayashide/ytlab_ros_ws/ytlab_hsr/ytlab_hsr_modules/datas/z2hzH.csv",z,delimiter=",")
+np.savetxt("/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/scripts/monitor/t2hzH.csv",t,delimiter=",")
+np.savetxt("/home/hayashide/ytlab_ros_ws/ytlab_hsr/ytlab_hsr_modules/datas/t2hzH.csv",t,delimiter=",")
