c9ed54f508466e5a1c3d68c450f100ea3942358a
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-01-11 19:39:01 +0900

    add vicon

--- sotsuron_experiment/scripts/bag_to_path.py
+++ sotsuron_experiment/scripts/bag_to_path.py
@@ -7,10 +7,10 @@ from glob import glob
 
 bags=sorted(glob("/media/hayashide/KIOXIA/hayashide/rosbag/0108/yoshinari/*"))
 
-for bag in bags:
+for bag in bags[12:]:
     print(bag)
     bag_basename=os.path.basename(bag)
-    csv_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0108/"+bag_basename[:-4]+"_slow.csv"
+    csv_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0108/csv/"+bag_basename[:-4]+"_slow.csv"
     play_cmd=f"roslaunch sotsuron_experiment path_plotter.launch bag_basename:={bag_basename} bag_path:={bag} csv_path:={csv_path}"
     # runcmd=sp.call(cmd.split())
     # print(runcmd)
--- sotsuron_experiment/scripts/path_plotter.py
+++ sotsuron_experiment/scripts/path_plotter.py
@@ -4,35 +4,51 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from glob import glob
+csv_dir_path=os.environ['HOME']+"/catkin_ws/src/sotsuron_experiment/results/0108/csv"
+csv_result_path=os.environ['HOME']+"/catkin_ws/src/sotsuron_experiment/results/0108/results"
 
-csv_path=os.environ['HOME']+"/catkin_ws/src/sotsuron_experiment/gaits/0105_cover.csv"
-data=np.loadtxt(csv_path,delimiter=",")
+csv_paths=sorted(glob(csv_dir_path+"/*"))
+for csv_path in csv_paths:
+    data=np.loadtxt(csv_path,delimiter=",")
 
-t_img=data[:,0]
-x=data[:,1]/1000
-y=data[:,2]/1000
-z=data[:,3]/1000
-t_odm=data[:,5]
-xR=data[:,6]
-yR=data[:,7]
-thR=data[:,8]
-pan=data[:,9]
+    t_img=data[:,0]
+    x=data[:,1]/1000
+    y=data[:,2]/1000
+    z=data[:,3]/1000
+    t_odm=data[:,5]
+    xR=data[:,6]
+    yR=data[:,7]
+    thR=data[:,8]
+    pan=data[:,9]
 
-xH=xR+z*np.cos(thR+pan)+x*np.sin(thR+pan)
-yH=yR+z*np.sin(thR+pan)-x*np.cos(thR+pan)
+    xH=xR+z*np.cos(thR+pan)+x*np.sin(thR+pan)
+    yH=yR+z*np.sin(thR+pan)-x*np.cos(thR+pan)
 
-# plt.plot(t_img,x,label="x")
-# plt.plot(t_img,y,label="y")
-# plt.plot(t_img,z,label="z")
-# plt.plot(t_odm,xR,label="xR")
-# plt.plot(t_odm,yR,label="yR")
-# plt.plot(t_odm,thR,label="thR")
-# plt.plot(t_img,xH,label="xH")
-# plt.plot(t_img,yH,label="yH")
-# plt.plot(t_img,t_odm-t_img,label="time gap")
 
-plt.plot(xR,yR,label="HSR position by odometry")
-plt.plot(xH,yH,label="gravity zone of the human")
-plt.plot([-2,7],[0,0],label="truth path of the human")
-plt.legend()
-plt.show()
+    plt.plot(xR,yR,label="HSR position by odometry")
+    plt.plot(xH,yH,label="gravity zone of the human")
+    np.savetxt(csv_result_path+"/csv/"+os.path.basename(csv_path[:-4])+"_results.csv",np.column_stack((xR,yR,xH,yH)),delimiter=",")
+
+    if "_0_" not in os.path.basename(csv_path):
+        calib_data=np.loadtxt(csv_result_path+"/csv/20230107_rotation_0_18_yoshinari_yoko_slow_results.csv",delimiter=",")
+        err_th=np.arctan(np.average(calib_data[:,2]/1000)/np.average(calib_data[:,3]/1000))
+        xH_clbd=xR+z*np.cos(thR+pan+err_th)+x*np.sin(thR+pan+err_th)
+        yH_clbd=yR+z*np.sin(thR+pan+err_th)-x*np.cos(thR+pan+err_th)
+        plt.plot(xH_clbd,yH_clbd,label="gravity zone of the human (calibrated)")
+
+
+    # plt.plot(t_img,x,label="x")
+    # plt.plot(t_img,y,label="y")
+    # plt.plot(t_img,z,label="z")
+    # plt.plot(t_odm,xR,label="xR")
+    # plt.plot(t_odm,yR,label="yR")
+    # plt.plot(t_odm,thR,label="thR")
+    # plt.plot(t_img,xH,label="xH")
+    # plt.plot(t_img,yH,label="yH")
+    # plt.plot(t_img,t_odm-t_img,label="time gap")
+    # plt.plot([-2,7],[0,0],label="truth path of the human")
+    plt.legend()
+    plt.title(os.path.basename(csv_path[:-4]))
+    plt.savefig(csv_result_path+"/graph/"+os.path.basename(csv_path[:-4])+".png")
+    plt.cla()
