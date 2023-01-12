07b23e39633d74af188c5f6e36056d2e22a6d113
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-01-12 10:08:51 +0900

    renamed results

--- sotsuron_experiment/scripts/path_plotter.py
+++ sotsuron_experiment/scripts/path_plotter.py
@@ -5,7 +5,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from glob import glob
-csv_dir_path=os.environ['HOME']+"/catkin_ws/src/sotsuron_experiment/results/0108/csv"
+# csv_dir_path=os.environ['HOME']+"/catkin_ws/src/sotsuron_experiment/results/0108/csv"
+csv_dir_path=os.environ['HOME']+"/catkin_ws/src/sotsuron_experiment/results/0108/csv/renamed"
 csv_result_path=os.environ['HOME']+"/catkin_ws/src/sotsuron_experiment/results/0108/results"
 
 csv_paths=sorted(glob(csv_dir_path+"/*"))
@@ -28,23 +29,23 @@ for csv_path in csv_paths:
 
     plt.plot(xR,yR,label="HSR position by odometry")
     plt.plot(xH,yH,label="gravity zone of the human")
-    np.savetxt(csv_result_path+"/csv/"+os.path.basename(csv_path[:-4])+"_results.csv",np.column_stack((xR,yR,xH,yH)),delimiter=",")
+    np.savetxt(csv_result_path+"/csv/"+os.path.basename(csv_path[:-8])+"_results.csv",np.column_stack((xR,yR,xH,yH)),delimiter=",")
 
-    if "_0_" not in os.path.basename(csv_path):
-        calib_data=np.loadtxt(csv_result_path+"/csv/20230107_rotation_0_18_yoshinari_yoko_slow_results.csv",delimiter=",")
-        err_th=np.arctan(np.average(calib_data[:,2]/1000)/np.average(calib_data[:,3]/1000))
-        xH_clbd=xR+z*np.cos(thR+pan+err_th)+x*np.sin(thR+pan+err_th)
-        yH_clbd=yR+z*np.sin(thR+pan+err_th)-x*np.cos(thR+pan+err_th)
-        plt.plot(xH_clbd,yH_clbd,label="gravity zone of the human (calibrated)")
+    # if "_0_" not in os.path.basename(csv_path):
+    #     calib_data=np.loadtxt(csv_result_path+"/csv/20230107_rotation_0_18_yoshinari_yoko_slow_results.csv",delimiter=",")
+    #     err_th=np.arctan(np.average(calib_data[:,2]/1000)/np.average(calib_data[:,3]/1000))
+    #     xH_clbd=xR+z*np.cos(thR+pan+err_th)+x*np.sin(thR+pan+err_th)
+    #     yH_clbd=yR+z*np.sin(thR+pan+err_th)-x*np.cos(thR+pan+err_th)
+    #     plt.plot(xH_clbd,yH_clbd,label="gravity zone of the human (calibrated)")
 
 
     # VICON data import
 
-    
+
 
     xR_HSR=np.average(xR)
     yR_HSR=np.average(yR)
-    xR_VCN=np.average()
+    # xR_VCN=np.average()
     
 
     # plt.plot(t_img,x,label="x")
@@ -59,5 +60,5 @@ for csv_path in csv_paths:
     # plt.plot([-2,7],[0,0],label="truth path of the human")
     plt.legend()
     plt.title(os.path.basename(csv_path[:-4]))
-    plt.savefig(csv_result_path+"/graph/"+os.path.basename(csv_path[:-4])+".png")
+    plt.savefig(csv_result_path+"/graph/"+os.path.basename(csv_path[:-8])+".png")
     plt.cla()
