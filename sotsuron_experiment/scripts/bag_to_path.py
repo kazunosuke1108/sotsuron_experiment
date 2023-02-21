4a6c4e20b53ae6abf7fea77b6ef6bb677c8a3463
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-02-21 17:11:19 +0900

    WIP

--- sotsuron_experiment/scripts/bag_to_path.py
+++ sotsuron_experiment/scripts/bag_to_path.py
@@ -7,23 +7,23 @@ from glob import glob
 
 # bags=sorted(glob("/media/hayashide/KIOXIA/hayashide/rosbag/0203/*"))
 # bags=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_hsr/ytlab_hsr_modules/rosbag/EtoE/*"))
-bags=sorted(glob("/media/hayashide/KIOXIA/hayashide/rosbag/0214/*"))
+bags=sorted(glob("/media/hayashide/KIOXIA/hayashide/rosbag/0220/*"))
 print(bags)
 
 partial_bags=[]
 for bag in bags:
-    if "_02_" in bag:
+    if "_23_" in bag or "_24_" in bag or "_26_" in bag:
         partial_bags.append(bag)
 
 for bag in partial_bags:
 # for bag in bags:
     print(bag)
     bag_basename=os.path.basename(bag)
-    csv_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0214/csv/"+bag_basename[:-4]+".csv"
-    odom_csv_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0214/odom_csv/"+bag_basename[:-4]+".csv"
-    avi_dir_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0214/movie/"
-    avi_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0214/movie/"+bag_basename[:-4]+".avi"
-    play_cmd=f"roslaunch sotsuron_experiment path_plotter.launch avi_basename:={bag_basename[:-4]} bag_basename:={bag_basename} bag_path:={bag} csv_path:={csv_path} odom_csv_path:={odom_csv_path} save_path:={avi_path}"
+    csv_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0220/csv/"+bag_basename[:-4]+".csv"
+    odom_csv_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0220/odom_csv/"+bag_basename[:-4]+".csv"
+    avi_dir_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0220/movie/"
+    avi_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/0220/movie/"+bag_basename[:-4]+".avi"
+    play_cmd=f"roslaunch sotsuron_experiment path_plotter.launch avi_basename:={bag_basename[:-4]} bag_basename:={bag_basename[:-4]} bag_path:={bag} csv_path:={csv_path} odom_csv_path:={odom_csv_path} save_path:={avi_path}"
     # runcmd=sp.call(cmd.split())
     # print(runcmd)
     os.system(play_cmd)
@@ -32,7 +32,7 @@ for bag in partial_bags:
     os.system(graph_cmd)
 
     mp4_cmd=f"python3 conv_avi_turn_mp4.py {avi_dir_path}"
-    # os.system(mp4_cmd)
+    os.system(mp4_cmd)
 
 
 """
