c982d69f8b7ce9001f8a5b2eba697a7267431540
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-06-01 14:30:00 +0900

    label

---
+++ sotsuron_experiment/matlab_ws/getLabel.m
@@ -0,0 +1,3 @@
+jointname=["nose","l-eye","r-eye","l-ear","r-ear","l-shoulder","r-shoulder","l-elbow","r-elbow","l-hand","r-hand","l-base","r-base","l-ankle","r-ankle","l-foot","r-foot"]
+
+labelList=[jointname+string("-x"),jointname+string("-y"),jointname+string("-z"),"x_R","y_R","theta_R","pan_R","t"]
