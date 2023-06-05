7e3b766c96fff7f56af18a0e7a3ac7116e249b92
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-06-05 19:05:01 +0900

    add data

--- sotsuron_experiment/matlab_ws/test.m
+++ sotsuron_experiment/matlab_ws/test.m
@@ -1,8 +1,7 @@
 clc;clear;
-
-plot3([0,1;2,2;1,0],[2,0;1,2;1,0],[0,1;0,2;1,0],)
-grid on
-xlabel("x [m]")
-ylabel("y [m]")
-zlabel("z [m]")
-[0,1;2,2;1,0],[2,0;1,2;1,0],[0,1;0,2;1,0]
+mat_general=load("C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\result.csv");
+fig=figure();clf;
+plot(1:length(mat_general),mat_general(:,end)-mat_general(1,end))
+xlabel("frame no.")
+ylabel("time [s]")
+saveas(fig,"C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\time_noise.png")
