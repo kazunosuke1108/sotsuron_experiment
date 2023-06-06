b083ddc05609db3c6ad2f0b7134f1fdce3cadc86
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-06-06 18:06:15 +0900

    model based control

--- sotsuron_experiment/matlab_ws/ankle.m
+++ sotsuron_experiment/matlab_ws/ankle.m
@@ -1,45 +1,46 @@
 mat_general=readmatrix("C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\results\0117\csv\20230117_d_060_1_Hayashide.csv");
 mat=readmatrix("C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\result.csv")
 labelList=getLabel();
-rajc.no=15;
-lajc.no=14;
+r.no=13;
+l.no=12;
 
 % [~,ind] = sort(mat(:,end));
 % mat= mat(ind,:);
-t=mat(:,56)-mat(1,56)
-rajc.mat=[mat(:,rajc.no),mat(:,rajc.no+17),mat(:,rajc.no+17*2)]
-lajc.mat=[mat(:,lajc.no),mat(:,lajc.no+17),mat(:,lajc.no+17*2)];
+% t=mat(:,56)-mat(1,56)
+t=fixTimeBug(mat);
+r.mat=[mat(:,r.no),mat(:,r.no+17),mat(:,r.no+17*2)]
+l.mat=[mat(:,l.no),mat(:,l.no+17),mat(:,l.no+17*2)];
 
 
 fig=figure();clf;
 % plot(1:length(t),t)
 
 subplot(2,2,1)
-plot(t,rajc.mat(:,1))
+plot(t,r.mat(:,1))
 hold on
-plot(t,lajc.mat(:,1))
-title(labelList(rajc.no))
+plot(t,l.mat(:,1))
+title(labelList(r.no))
 xlabel("time \it{t}\rm [s]",'FontName','Times New Roman')
 ylabel("x position \it{x}\rm [m]",'FontName','Times New Roman')
 
 subplot(2,2,2)
-plot(t,rajc.mat(:,2))
+plot(t,r.mat(:,2))
 hold on
-plot(t,lajc.mat(:,2))
-title(labelList(rajc.no+17))
+plot(t,l.mat(:,2))
+title(labelList(r.no+17))
 xlabel("time \it{t}\rm [s]",'FontName','Times New Roman')
 ylabel("y position \it{y}\rm [m]",'FontName','Times New Roman')
 
 subplot(2,2,3)
-plot(t,rajc.mat(:,3))
+plot(t,r.mat(:,3))
 hold on
-plot(t,lajc.mat(:,3))
-title(labelList(rajc.no+17*2))
+plot(t,l.mat(:,3))
+title(labelList(r.no+17*2))
 xlabel("time \it{t}\rm [s]",'FontName','Times New Roman')
 ylabel("z position \it{z}\rm [m]",'FontName','Times New Roman')
 
 subplot(2,2,4)
-plot3(rajc.mat(:,1),rajc.mat(:,2),rajc.mat(:,3))
+plot3(r.mat(:,1),r.mat(:,2),r.mat(:,3))
 grid on
 daspect([1 1 1])
 xlabel("position \it{x}\rm [m]",'FontName','Times New Roman')
@@ -47,4 +48,4 @@ ylabel("position \it{y}\rm [m]",'FontName','Times New Roman')
 zlabel("position \it{z}\rm [m]",'FontName','Times New Roman')
 
 % saveas(fig,"C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\"+"time_noise.png")
-saveas(fig,"C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\"+labelList(rajc.no)+".png")
+saveas(fig,"C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\"+labelList(r.no)+"timeFixed.png")
--- sotsuron_experiment/matlab_ws/fixTimeBug.m
+++ sotsuron_experiment/matlab_ws/fixTimeBug.m
@@ -1,9 +1,9 @@
-function time_compensated=fixTimeBug(mat_general)
-    % mat_general=load("C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\result.csv");
-    fig=figure();clf;
-    time_series=mat_general(:,end)-mat_general(1,end)
-    time_compensated=zeros(size(time_series))
-    frame_idx=1:length(time_series)
+function time_compensated=fixTimeBug(mat)
+    % mat=load("C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\result.csv");
+    % fig=figure();clf;
+    time_series=mat(:,end)-mat(1,end);
+    time_compensated=zeros(size(time_series));
+    frame_idx=1:length(time_series);
 
     time_compensated(1)=time_series(1);
     time_compensated(2)=time_series(2);
@@ -14,11 +14,11 @@ function time_compensated=fixTimeBug(mat_general)
     for i = 2:length(time_series)-1
         if time_series(i+1)-time_series(i)>2.5*(time_compensated(i)-time_compensated(i-1)) || time_series(i+1)-time_series(i)<0
             if ~outlier_flg
-                recent_outlier=frame_idx(i)
+                recent_outlier=frame_idx(i);
             end
             outlier_flg=true;
             time_compensated(i+1)=(time_series(recent_outlier)-time_series(recent_outlier-1))*(frame_idx(i+1)-frame_idx(i))+time_compensated(i);
-            scatter(frame_idx(i+1),time_compensated(i+1));
+            % scatter(frame_idx(i+1),time_compensated(i+1));
         else
             time_compensated(i+1)=time_series(i+1);
             hold on
@@ -27,7 +27,7 @@ function time_compensated=fixTimeBug(mat_general)
     end
 end
 % % time_compensated=smoothdata(time_series)
-% plot(frame_idx,mat_general(:,end)-mat_general(1,end))
+% plot(frame_idx,mat(:,end)-mat(1,end))
 % hold on
 % % plot(frame_idx,straight(1)*frame_idx)
 % plot(frame_idx,time_compensated)
