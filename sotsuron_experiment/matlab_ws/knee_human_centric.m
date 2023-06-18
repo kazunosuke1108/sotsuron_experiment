73756445fc3175a857b76086456d50160fadc14e
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-06-18 11:24:16 +0900

    add ocl ratio

--- sotsuron_experiment/matlab_ws/knee_human_centric.m
+++ sotsuron_experiment/matlab_ws/knee_human_centric.m
@@ -1,5 +1,6 @@
 clc;clear;
 close all;
+load("C:\Users\hayashide\kazu_ws\sotsuron_simulator\matlab_ws\230117\results\230117_7F\230117_200325_vx060_y050\230117_200325_.mat");
 mat_general=readmatrix("C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\results\0117\csv\20230117_d_060_1_Hayashide.csv");
 mat=readmatrix("C:\Users\hayashide\kazu_ws\sotsuron_experiment\sotsuron_experiment\matlab_ws\result.csv");
 labelList=getLabel();
@@ -19,6 +20,7 @@ distance=sqrt((r.mat(:,1)-l.mat(:,1)).^2+(r.mat(:,2)-l.mat(:,2)).^2+(r.mat(:,3)-
 not_ocl_idx=find(distance>=0.1);
 ocl_idx=find(distance<0.1);
 
+%% human centric plot
 knee_center=1/2*(r.mat+l.mat);
 
 r.relative_mat=r.mat-knee_center;
@@ -51,6 +53,100 @@ for i = 1:length(r.relative_mat)
     frames(i)=getframe(fig1);
 end
 v = VideoWriter('Hayashide_'+"human_centric"+'_knee.mp4','MPEG-4');
+v.FrameRate=1/0.05;
+open(v);
+writeVideo(v,frames);
+close(v);
+
+%% 角度と欠損のヒストグラム
+bins=0:10:180;
+clf;
+fig1 = figure(1);
+atan_list=rad2deg(atan(rbt.relative_mat(:,2)./rbt.relative_mat(:,1)))
+atan_list=atan_list.*(atan_list>=0)+(180+atan_list).*(atan_list<0)
+ocl_atan_list=rad2deg(atan(rbt.relative_mat(ocl_idx,2)./rbt.relative_mat(ocl_idx,1)));
+ocl_atan_list=ocl_atan_list.*(ocl_atan_list>=0)+(180+ocl_atan_list).*(ocl_atan_list<0)
+ocl_atan_list=ocl_atan_list+90*(ocl_atan_list<0);
+% subplot(2,2,1)
+% h_all=histogram(-atan_list,bins)
+% subplot(2,2,2)
+% h_ocl=histogram(-ocl_atan_list,bins)
+% h_ocl.Data
+% subplot(2,2,3)
+h_all=histogram(-atan_list,length(ocl_idx),"FaceColor","k")
+h_all_values=h_all.Values
+h_all_BinEdges=h_all.BinEdges
+hold on
+h_ocl=histogram(-ocl_atan_list,length(ocl_idx),"FaceColor","r")
+h_ocl_values=h_ocl.Values
+h_ocl_BinEdges=h_ocl.BinEdges
+hold on
+plot(h_ocl_BinEdges(1:length(h_ocl_values)),h_ocl_values./h_all_values,"oy-","LineWidth",2)
+xlabel("人の進行方向を -180 deg とした時の相対角度 [deg]")
+ylabel("フレーム数 [枚]")
+ylim([0,10])
+legend("全フレーム数","隠れ発生数","隠れ発生比率","Location","northwest")
+grid on
+saveas(fig1,"histgram.png")
+% subplot(2,2,4)
+
+%% robot centric plot
+
+r.relative_mat_rbt=zeros(length(r.mat),2)
+l.relative_mat_rbt=zeros(length(l.mat),2)
+for i = 1:length(r.relative_mat)
+    rotate_matrix=[cos(rbt.mat(i,3)+rbt.mat(i,4)),sin(rbt.mat(i,3)+rbt.mat(i,4));...
+        -sin(rbt.mat(i,3)+rbt.mat(i,4)),cos(rbt.mat(i,3)+rbt.mat(i,4))];
+    r.relative_mat_rbt(i,:)=transpose(rotate_matrix*[r.mat(i,1)-rbt.mat(i,1);r.mat(i,2)-rbt.mat(i,2)]);
+    l.relative_mat_rbt(i,:)=transpose(rotate_matrix*[l.mat(i,1)-rbt.mat(i,1);l.mat(i,2)-rbt.mat(i,2)]);
+end
+
+fig2=figure(2);
+fig2.Position=[100 100 700 800];
+frames(length(knee_center)) = struct('cdata',[],'colormap',[]);
+hold on;
+grid on;
+daspect([1 1 1]);
+xlabel("position x [m]");
+ylabel("position y [m]");
+title("robot centric view");
+
+arc_resolution=100;
+arc_rad = linspace(-sns.phi,sns.phi,arc_resolution);
+arc_r1_x = sns.r1*cos(arc_rad);
+arc_r1_y = sns.r1*sin(arc_rad);
+arc_r2_x = sns.r2*cos(arc_rad);
+arc_r2_y = sns.r2*sin(arc_rad);
+arc_r1 = plot(arc_r1_x,arc_r1_y,'g');
+arc_r2 = plot(arc_r2_x,arc_r2_y,'g');
+arc_right = plot([arc_r1_x(1),arc_r2_x(1)],[arc_r1_y(1),arc_r2_y(1)],'g');
+arc_left = plot([arc_r1_x(end),arc_r2_x(end)],[arc_r1_y(end),arc_r2_y(end)],'g');
+arc_right_helper=plot([0,arc_r1_x(1)],[0,arc_r1_y(1)],'--g');
+arc_left_helper=plot([0,arc_r1_x(end)],[0,arc_r1_y(end)],'--g');
+rbt_position=rectangle('Position',[0-0.3,0-0.3,0.6,0.6],'Curvature',[1 1],'EdgeColor','b');
+rbt_direction=plot([0,0.3],[0,0],'b','LineWidth',2);
+
+r_plot=plot(r.relative_mat_rbt(1,1),r.relative_mat_rbt(1,2),"r","LineWidth",1.5)
+l_plot=plot(l.relative_mat_rbt(1,1),l.relative_mat_rbt(1,2),"b","LineWidth",1.5)
+r_plot_hist=plot(r.relative_mat_rbt(1,1),r.relative_mat_rbt(1,2),"r","LineWidth",0.25)
+l_plot_hist=plot(l.relative_mat_rbt(1,1),l.relative_mat_rbt(1,2),"b","LineWidth",0.25)
+
+ocl_plot=plot(r.relative_mat_rbt(1,1),rbt.relative_mat(plt_ocl_idx,2),"xk","MarkerSize",10);
+
+frames(1)=getframe(fig2);
+
+for i = 1:length(r.relative_mat_rbt)
+    plt_ocl_idx=ocl_idx(find(ocl_idx<=i));
+    set(ocl_plot,"XData",l.relative_mat_rbt(plt_ocl_idx,1),"YData",l.relative_mat_rbt(plt_ocl_idx,2));
+    set(r_plot,"XData",r.relative_mat_rbt(max(i-5,1):i,1),"YData",r.relative_mat_rbt(max(i-5,1):i,2));
+    set(l_plot,"XData",l.relative_mat_rbt(max(i-5,1):i,1),"YData",l.relative_mat_rbt(max(i-5,1):i,2));
+    set(r_plot_hist,"XData",r.relative_mat_rbt(1:i,1),"YData",r.relative_mat_rbt(1:i,2));
+    set(l_plot_hist,"XData",l.relative_mat_rbt(1:i,1),"YData",l.relative_mat_rbt(1:i,2));
+    drawnow
+    frames(i)=getframe(fig2);
+end
+
+v = VideoWriter('Hayashide_'+"robot_centric"+'_knee.mp4','MPEG-4');
 v.FrameRate=1/0.05;
 open(v);
 writeVideo(v,frames);
