52162b543bb7872ce2748e6c9034c934e3a4e2ea
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-10-22 19:28:05 +0900

    AutoPush no.0/10

--- sotsuron_experiment/scripts/analysis_main.py
+++ sotsuron_experiment/scripts/analysis_main.py
@@ -6,8 +6,9 @@ from git_auto_push import git_auto_push
 
 # この手前まで
 # /home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231020/csv/09_07_00__2023-10-21-17-10-43_tf.csv
-bags=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/04*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/05*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/06*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/07*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/08*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09*"))
-pprint(bags)
+bags=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_07_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_08_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_09_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_10_*"))
+bags+=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/10*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/11*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/12*"))
+# pprint(bags)
 for bag in bags:
     launch_command = f"roslaunch sotsuron_experiment ras_postprocessor.launch bag_name:={os.path.basename(bag)[:-4]}"
     subprocess.run(launch_command, shell=True, check=True)
--- sotsuron_experiment/scripts/git_auto_push.py
+++ sotsuron_experiment/scripts/git_auto_push.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import time
 import subprocess
 
 def git_auto_push(msg="AutoPush"):
@@ -8,3 +8,9 @@ def git_auto_push(msg="AutoPush"):
     subprocess.run(f"git commit -m \"{msg}\"", shell=True, check=True)
     subprocess.run("git pull origin devel/ras", shell=True, check=True)
     subprocess.run("git push origin devel/ras", shell=True, check=True)
+
+interval_sec=60*60*2
+num_iter=10
+for i in range(num_iter):
+    git_auto_push(f"AutoPush no.{i}/{num_iter}")
+    time.sleep(interval_sec)
