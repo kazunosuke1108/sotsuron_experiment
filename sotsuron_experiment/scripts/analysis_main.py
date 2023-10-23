9985819b1569da1865d842e17393c0f6733e300f
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-10-23 23:35:58 +0900

    recover 03_00_00

--- sotsuron_experiment/scripts/analysis_main.py
+++ sotsuron_experiment/scripts/analysis_main.py
@@ -2,13 +2,15 @@ from glob import glob
 from pprint import pprint
 import os
 import subprocess
-from git_auto_push import git_auto_push
+# from git_auto_push import git_auto_push
 
 # この手前まで
 # /home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231020/csv/09_07_00__2023-10-21-17-10-43_tf.csv
-bags=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_07_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_08_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_09_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_10_*"))
-bags+=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/10*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/11*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/12*"))
+# bags=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_07_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_08_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_09_*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/09_10_*"))
+# bags+=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/10*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/11*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/12*"))
 # pprint(bags)
+bags=["/home/hayashide/catkin_ws/media/hayashide/KIOXIA/hayashide/rosbag/20231020/03_00_00__2023-10-20-17-09-06.bag"]
+# bags+=sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/13*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/14*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/15*"))+sorted(glob("/home/hayashide/catkin_ws/src/ytlab_rosbag/rosbag/16*"))
 for bag in bags:
     launch_command = f"roslaunch sotsuron_experiment ras_postprocessor.launch bag_name:={os.path.basename(bag)[:-4]}"
     subprocess.run(launch_command, shell=True, check=True)
--- sotsuron_experiment/scripts/test.py
+++ sotsuron_experiment/scripts/test.py
@@ -1,17 +1,27 @@
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
+# import time
+# while True:
+csvpath="/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231020/csv/03_00_00__2023-10-20-17-09-06_tf.csv"
+data=pd.read_csv(csvpath)#,names=["tH","x","y","z","0","tR","xR","yR","thR","panR"])
+data=data.to_numpy()
+sort_idx=np.argsort(data,axis=0)[:,0]
+print(np.argsort(data,axis=0)[:,0])
+data=data[sort_idx,:]
+# data=np.sort(data,axis=0)
+# data.sort(axis=0)
+# data=data[::-1]
+# print(data["tH"])
 
-csvpath="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231014/csv/_2023-10-14-19-43-12.bag_tf.csv"
-data=pd.read_csv(csvpath,names=["t","x","y","z"])
-
-
-
-plt.plot(data["t"],data["x"],label="x")
-plt.plot(data["t"],data["y"],label="y")
-plt.plot(data["t"],data["z"],label="z")
+plt.plot(data[:,0],data[:,-3],label="x")#,s=3)
+plt.plot(data[:,0],data[:,-3-3],label="y")#,s=3)
+# plt.scatter(data[:,0],data[:,3],label="z",s=3)
 plt.legend()
 plt.grid()
 plt.savefig(csvpath[:-4]+".png")
 plt.show()
+# plt.cla()
+# time.sleep(1)
