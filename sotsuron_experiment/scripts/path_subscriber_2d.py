ea86542d73a58c449ae4f87152898c8ad10cb286
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-10-24 16:50:46 +0900

    WIP

--- sotsuron_experiment/scripts/path_subscriber_2d.py
+++ sotsuron_experiment/scripts/path_subscriber_2d.py
@@ -100,135 +100,135 @@ def detect_kp(rgb_array):
     except IndexError:
         return [None]
 
-# def get_position_kp(rgb_array,dpt_array,np_pred_keypoints,proj_mtx):
-#     # size translation
-#     y_rgb2dpt=dpt_array.shape[0]/rgb_array.shape[0]
-#     x_rgb2dpt=dpt_array.shape[1]/rgb_array.shape[1]
+def get_position_kp(rgb_array,dpt_array,np_pred_keypoints,proj_mtx):
+    # size translation
+    y_rgb2dpt=dpt_array.shape[0]/rgb_array.shape[0]
+    x_rgb2dpt=dpt_array.shape[1]/rgb_array.shape[1]
 
-#     pred_keypoints_3D=[]
+    pred_keypoints_3D=[]
 
-#     for i, kp in enumerate(np_pred_keypoints):
-#         kp_0=kp[0]*y_rgb2dpt
-#         kp_1=kp[1]*x_rgb2dpt
-#         size_bdbox=30
-#         dpt=np.nanmedian(dpt_array[int(kp_1)-size_bdbox:int(kp_1)+size_bdbox,int(kp_0)-size_bdbox:int(kp_0)+size_bdbox])
-#         # rospy.loginfo(f"{kp_0},{kp_1},{dpt}")
-#         # rospy.loginfo(dpt_array[int(kp_0)-size_bdbox:int(kp_0)+size_bdbox,int(kp_1)-size_bdbox:int(kp_1)+size_bdbox])
-#         kp_3d=dpt*np.dot(np.linalg.pinv(proj_mtx),np.array([kp_0,kp_1,1]).T)
-#         # rospy.loginfo(dpt)
-#         pred_keypoints_3D.append(kp_3d.tolist())
-#     np_pred_keypoints_3D=np.array(pred_keypoints_3D)
-#     return np_pred_keypoints_3D
+    for i, kp in enumerate(np_pred_keypoints):
+        kp_0=kp[0]*y_rgb2dpt
+        kp_1=kp[1]*x_rgb2dpt
+        size_bdbox=30
+        dpt=np.nanmedian(dpt_array[int(kp_1)-size_bdbox:int(kp_1)+size_bdbox,int(kp_0)-size_bdbox:int(kp_0)+size_bdbox])
+        # rospy.loginfo(f"{kp_0},{kp_1},{dpt}")
+        # rospy.loginfo(dpt_array[int(kp_0)-size_bdbox:int(kp_0)+size_bdbox,int(kp_1)-size_bdbox:int(kp_1)+size_bdbox])
+        kp_3d=dpt*np.dot(np.linalg.pinv(proj_mtx),np.array([kp_0,kp_1,1]).T)
+        # rospy.loginfo(dpt)
+        pred_keypoints_3D.append(kp_3d.tolist())
+    np_pred_keypoints_3D=np.array(pred_keypoints_3D)
+    return np_pred_keypoints_3D
 
-# def get_gravity_zone(np_pred_keypoints_3D):
-#     mass={
-#         "head":0.044,
-#         "neck":0.033,
-#         "body":0.479,
-#         "upper_arm":0.0265,
-#         "lower_arm":0.015,
-#         "hand":0.009,
-#         "upper_leg":0.1,
-#         "lower_leg":0.0535,
-#         "foot":0.019
-#     }
-#     total_mass=mass["head"]+mass["neck"]+mass["body"]+2*(mass["upper_arm"]+mass["lower_arm"]+mass["hand"]+mass["upper_leg"]+mass["lower_leg"]+mass["foot"])
-#     position={
-#         "head":1/5*(np_pred_keypoints_3D[0]+np_pred_keypoints_3D[1]+np_pred_keypoints_3D[2]+np_pred_keypoints_3D[3]+np_pred_keypoints_3D[4]),
-#         "neck":1/3*(np_pred_keypoints_3D[0]+np_pred_keypoints_3D[1]+np_pred_keypoints_3D[2]),
-#         "body":1/4*(np_pred_keypoints_3D[5]+np_pred_keypoints_3D[6]+np_pred_keypoints_3D[11]+np_pred_keypoints_3D[12]),
-#         "upper_arm_L":1/2*(np_pred_keypoints_3D[5]+np_pred_keypoints_3D[7]),
-#         "upper_arm_R":1/2*(np_pred_keypoints_3D[6]+np_pred_keypoints_3D[8]),
-#         "lower_arm_L":1/2*(np_pred_keypoints_3D[7]+np_pred_keypoints_3D[9]),
-#         "lower_arm_R":1/2*(np_pred_keypoints_3D[8]+np_pred_keypoints_3D[10]),
-#         "hand_L":np_pred_keypoints_3D[9],
-#         "hand_R":np_pred_keypoints_3D[10],
-#         "upper_leg_L":1/2*(np_pred_keypoints_3D[11]+np_pred_keypoints_3D[13]),
-#         "upper_leg_R":1/2*(np_pred_keypoints_3D[12]+np_pred_keypoints_3D[14]),
-#         "lower_leg_L":1/2*(np_pred_keypoints_3D[13]+np_pred_keypoints_3D[15]),
-#         "lower_leg_R":1/2*(np_pred_keypoints_3D[14]+np_pred_keypoints_3D[16]),
-#         "foot_L":np_pred_keypoints_3D[15],
-#         "foot_R":np_pred_keypoints_3D[16]
-#     }
-#     part_gravity={
-#         "head":mass['head']*position['head'],
-#         "neck":mass['neck']*position['neck'],
-#         "body":mass['body']*position['body'],
-#         "upper_arm":mass['upper_arm']*(position['upper_arm_L']+position['upper_arm_R']),
-#         "lower_arm":mass['lower_arm']*(position['lower_arm_L']+position['lower_arm_R']),
-#         "hand":mass['hand']*(position['hand_L']+position['hand_R']),
-#         "upper_leg":mass['upper_leg']*(position['upper_leg_L']+position['upper_leg_R']),
-#         "lower_leg":mass['lower_leg']*(position['lower_leg_L']+position['lower_leg_R']),
-#         "foot":mass['foot']*(position['foot_L']+position['foot_R'])
-#     }
+def get_gravity_zone(np_pred_keypoints_3D):
+    mass={
+        "head":0.044,
+        "neck":0.033,
+        "body":0.479,
+        "upper_arm":0.0265,
+        "lower_arm":0.015,
+        "hand":0.009,
+        "upper_leg":0.1,
+        "lower_leg":0.0535,
+        "foot":0.019
+    }
+    total_mass=mass["head"]+mass["neck"]+mass["body"]+2*(mass["upper_arm"]+mass["lower_arm"]+mass["hand"]+mass["upper_leg"]+mass["lower_leg"]+mass["foot"])
+    position={
+        "head":1/5*(np_pred_keypoints_3D[0]+np_pred_keypoints_3D[1]+np_pred_keypoints_3D[2]+np_pred_keypoints_3D[3]+np_pred_keypoints_3D[4]),
+        "neck":1/3*(np_pred_keypoints_3D[0]+np_pred_keypoints_3D[1]+np_pred_keypoints_3D[2]),
+        "body":1/4*(np_pred_keypoints_3D[5]+np_pred_keypoints_3D[6]+np_pred_keypoints_3D[11]+np_pred_keypoints_3D[12]),
+        "upper_arm_L":1/2*(np_pred_keypoints_3D[5]+np_pred_keypoints_3D[7]),
+        "upper_arm_R":1/2*(np_pred_keypoints_3D[6]+np_pred_keypoints_3D[8]),
+        "lower_arm_L":1/2*(np_pred_keypoints_3D[7]+np_pred_keypoints_3D[9]),
+        "lower_arm_R":1/2*(np_pred_keypoints_3D[8]+np_pred_keypoints_3D[10]),
+        "hand_L":np_pred_keypoints_3D[9],
+        "hand_R":np_pred_keypoints_3D[10],
+        "upper_leg_L":1/2*(np_pred_keypoints_3D[11]+np_pred_keypoints_3D[13]),
+        "upper_leg_R":1/2*(np_pred_keypoints_3D[12]+np_pred_keypoints_3D[14]),
+        "lower_leg_L":1/2*(np_pred_keypoints_3D[13]+np_pred_keypoints_3D[15]),
+        "lower_leg_R":1/2*(np_pred_keypoints_3D[14]+np_pred_keypoints_3D[16]),
+        "foot_L":np_pred_keypoints_3D[15],
+        "foot_R":np_pred_keypoints_3D[16]
+    }
+    part_gravity={
+        "head":mass['head']*position['head'],
+        "neck":mass['neck']*position['neck'],
+        "body":mass['body']*position['body'],
+        "upper_arm":mass['upper_arm']*(position['upper_arm_L']+position['upper_arm_R']),
+        "lower_arm":mass['lower_arm']*(position['lower_arm_L']+position['lower_arm_R']),
+        "hand":mass['hand']*(position['hand_L']+position['hand_R']),
+        "upper_leg":mass['upper_leg']*(position['upper_leg_L']+position['upper_leg_R']),
+        "lower_leg":mass['lower_leg']*(position['lower_leg_L']+position['lower_leg_R']),
+        "foot":mass['foot']*(position['foot_L']+position['foot_R'])
+    }
 
-#     gravity_zone=1/total_mass*(part_gravity["head"]+part_gravity["neck"]+part_gravity["body"]+part_gravity["upper_arm"]+part_gravity["lower_arm"]+part_gravity["hand"]+part_gravity["upper_leg"]+part_gravity["lower_leg"]+part_gravity["foot"])
-#     rospy.loginfo(gravity_zone)
-#     return gravity_zone
+    gravity_zone=1/total_mass*(part_gravity["head"]+part_gravity["neck"]+part_gravity["body"]+part_gravity["upper_arm"]+part_gravity["lower_arm"]+part_gravity["hand"]+part_gravity["upper_leg"]+part_gravity["lower_leg"]+part_gravity["foot"])
+    rospy.loginfo(gravity_zone)
+    return gravity_zone
 
-# def savefig(rgb_array,np_pred_keypoints):
-#     img=rgb_array
-#     remap_img=results_path+"/remap/"+str(time.time())+".png"
+def savefig(rgb_array,np_pred_keypoints):
+    img=rgb_array
+    remap_img=results_path+"/remap/"+str(time.time())+".png"
 
-#     for i,keypoint in enumerate(np_pred_keypoints):
-#         if float(keypoint[2])<0.4:
-#             paintCol=(0,0,255*(float(keypoint[2])+0.4))
-#             pass
-#         elif float(keypoint[2])<0.7:
-#             paintCol=(0,255*(float(keypoint[2])+0.7),0)
-#             pass
-#         else:
-#             paintCol=(255*(float(keypoint[2])),0,0)
-#             pass
-#         cv2.circle(img,
-#             center=(int(keypoint[0]), int(keypoint[1])),
-#             radius=3,
-#             color=paintCol,
-#             thickness=1,
-#             lineType=cv2.LINE_4,
-#             shift=0)
-#         cv2.putText(img,
-#                 text=str(round(float(keypoint[2]),3)),
-#                 org=(int(keypoint[0])+10, int(keypoint[1])),
-#                 fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-#                 fontScale=0.7,
-#                 color=paintCol,
-#                 thickness=1,
-#                 lineType=cv2.LINE_4)
-#         cv2.putText(img,
-#                 text=str(i),
-#                 org=(int(keypoint[0])-30, int(keypoint[1])),
-#                 fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-#                 fontScale=0.7,
-#                 color=paintCol,
-#                 thickness=2,
-#                 lineType=cv2.LINE_4)
+    for i,keypoint in enumerate(np_pred_keypoints):
+        if float(keypoint[2])<0.4:
+            paintCol=(0,0,255*(float(keypoint[2])+0.4))
+            pass
+        elif float(keypoint[2])<0.7:
+            paintCol=(0,255*(float(keypoint[2])+0.7),0)
+            pass
+        else:
+            paintCol=(255*(float(keypoint[2])),0,0)
+            pass
+        cv2.circle(img,
+            center=(int(keypoint[0]), int(keypoint[1])),
+            radius=3,
+            color=paintCol,
+            thickness=1,
+            lineType=cv2.LINE_4,
+            shift=0)
+        cv2.putText(img,
+                text=str(round(float(keypoint[2]),3)),
+                org=(int(keypoint[0])+10, int(keypoint[1])),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.7,
+                color=paintCol,
+                thickness=1,
+                lineType=cv2.LINE_4)
+        cv2.putText(img,
+                text=str(i),
+                org=(int(keypoint[0])-30, int(keypoint[1])),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.7,
+                color=paintCol,
+                thickness=2,
+                lineType=cv2.LINE_4)
 
-#     cv2.imwrite(remap_img,img)
+    cv2.imwrite(remap_img,img)
 
-# def prepare_tf(np_pred_keypoints_3D,rgb_data_header_stamp):
-#     tf_kp_list=[]
-#     for i,kp_3d in enumerate(np_pred_keypoints_3D):
-#         tf_name=f"hmn_joint_{str(i+1).zfill(2)}"
-#         t = geometry_msgs.msg.TransformStamped()
-#         # t.header.frame_id = "zed_left"
-#         t.header.frame_id = "zed_left_optical"
-#         t.header.stamp = rgb_data_header_stamp#rospy.Time.now()
-#         t.child_frame_id = tf_name
-#         if not np.isnan(kp_3d[0]):
-#             # t.transform.translation.x = gravity_zone[2]/1000
-#             # t.transform.translation.y = -gravity_zone[0]/1000
-#             # t.transform.translation.z = gravity_zone[1]/1000
-#             # if not np.isnan(gravity_zone[0]):
-#             t.transform.translation.x = kp_3d[0]/1000
-#             t.transform.translation.y = kp_3d[1]/1000
-#             t.transform.translation.z = kp_3d[2]/1000
-#             t.transform.rotation.x = 0.0
-#             t.transform.rotation.y = 0.0
-#             t.transform.rotation.z = 0.0
-#             t.transform.rotation.w = 1.0
-#             tf_kp_list.append(t)
-#     return tf_kp_list
+def prepare_tf(np_pred_keypoints_3D,rgb_data_header_stamp):
+    tf_kp_list=[]
+    for i,kp_3d in enumerate(np_pred_keypoints_3D):
+        tf_name=f"hmn_joint_{str(i+1).zfill(2)}"
+        t = geometry_msgs.msg.TransformStamped()
+        # t.header.frame_id = "zed_left"
+        t.header.frame_id = "zed_left_optical"
+        t.header.stamp = rgb_data_header_stamp#rospy.Time.now()
+        t.child_frame_id = tf_name
+        if not np.isnan(kp_3d[0]):
+            # t.transform.translation.x = gravity_zone[2]/1000
+            # t.transform.translation.y = -gravity_zone[0]/1000
+            # t.transform.translation.z = gravity_zone[1]/1000
+            # if not np.isnan(gravity_zone[0]):
+            t.transform.translation.x = kp_3d[0]/1000
+            t.transform.translation.y = kp_3d[1]/1000
+            t.transform.translation.z = kp_3d[2]/1000
+            t.transform.rotation.x = 0.0
+            t.transform.rotation.y = 0.0
+            t.transform.rotation.z = 0.0
+            t.transform.rotation.w = 1.0
+            tf_kp_list.append(t)
+    return tf_kp_list
 
 def ImageCallback_realsense(rgb_data,dpt_data,info_data,odm_data,joi_data):
     # if time.time()-start_time>10:
@@ -287,54 +287,54 @@ def ImageCallback_realsense(rgb_data,dpt_data,info_data,odm_data,joi_data):
     # rospy.loginfo(np_pred_keypoints)
 
     # 2D to 3D
-    # if len(np_pred_keypoints)>1:
-    #     # original_rgb_array_size=rgb_array.shape
-    #     # modified_rgb_array_size=(400,600)
-    #     # rgb_array=cv2.resize(rgb_array,dsize=(modified_rgb_array_size[1],modified_rgb_array_size[0]))
-    #     np_pred_keypoints_3D=get_position_kp(rgb_array,dpt_array,np_pred_keypoints,proj_mtx)
-    #     rospy.loginfo("####### debug ROI #######")
-    #     tf_kp_list=prepare_tf(np_pred_keypoints_3D,rgb_data.header.stamp)
-    #     print(np_pred_keypoints_3D.shape)
-    #     rospy.loginfo("####### debug ROI end #######")
-    #     keypoints_history.append(np_pred_keypoints_3D.reshape(-1).tolist())
+    if len(np_pred_keypoints)>1:
+        # original_rgb_array_size=rgb_array.shape
+        # modified_rgb_array_size=(400,600)
+        # rgb_array=cv2.resize(rgb_array,dsize=(modified_rgb_array_size[1],modified_rgb_array_size[0]))
+        np_pred_keypoints_3D=get_position_kp(rgb_array,dpt_array,np_pred_keypoints,proj_mtx)
+        rospy.loginfo("####### debug ROI #######")
+        tf_kp_list=prepare_tf(np_pred_keypoints_3D,rgb_data.header.stamp)
+        print(np_pred_keypoints_3D.shape)
+        rospy.loginfo("####### debug ROI end #######")
+        keypoints_history.append(np_pred_keypoints_3D.reshape(-1).tolist())
         
-    #     # gravity
-    #     gravity_zone=get_gravity_zone(np_pred_keypoints_3D)
+        # gravity
+        gravity_zone=get_gravity_zone(np_pred_keypoints_3D)
 
-    #     # publish gravity
-    #     t = geometry_msgs.msg.TransformStamped()
-    #     # t.header.frame_id = "zed_left"
-    #     t.header.frame_id = "zed_left_optical"
-    #     t.header.stamp = rgb_data.header.stamp#rospy.Time.now()
-    #     t.child_frame_id = "hmn"
-    #     if not np.isnan(gravity_zone[0]):
-    #         print(gravity_zone)
-    #         # t.transform.translation.x = gravity_zone[2]/1000
-    #         # t.transform.translation.y = -gravity_zone[0]/1000
-    #         # t.transform.translation.z = gravity_zone[1]/1000
-    #         # if not np.isnan(gravity_zone[0]):
-    #         t.transform.translation.x = gravity_zone[0]/1000
-    #         t.transform.translation.y = gravity_zone[1]/1000
-    #         t.transform.translation.z = gravity_zone[2]/1000
-    #         t.transform.rotation.x = 0.0
-    #         t.transform.rotation.y = 0.0
-    #         t.transform.rotation.z = 0.0
-    #         t.transform.rotation.w = 1.0
-    #         tfm = tf2_msgs.msg.TFMessage([t]+tf_kp_list)
-    #         pub_tf.publish(tfm)
-    #         # save gravity
-    #         gravity_zone=gravity_zone.tolist()
-    #         gravity_zone.insert(0,float(img_time_str))
-    #         gravity_zone.append(float(odm_time_str))
-    #         gravity_zone.append(_odom_x)
-    #         gravity_zone.append(_odom_y)
-    #         gravity_zone.append(_odom_theta)
-    #         gravity_zone.append(pan)
-    #         gravity_history.append(gravity_zone)
-    #         np.savetxt(csv_path[:-4]+"_kp.csv",keypoints_history,delimiter=",")
-    #         np.savetxt(csv_path,gravity_history,delimiter=",")
-    #     else:
-    #         print("!!!!!!!!!!!!!!!!!!!!!nan!!!!!!!!!!!!!!!!!!!!!")
+        # publish gravity
+        t = geometry_msgs.msg.TransformStamped()
+        # t.header.frame_id = "zed_left"
+        t.header.frame_id = "zed_left_optical"
+        t.header.stamp = rgb_data.header.stamp#rospy.Time.now()
+        t.child_frame_id = "hmn"
+        # if not np.isnan(gravity_zone[0]):
+        print(gravity_zone)
+        # t.transform.translation.x = gravity_zone[2]/1000
+        # t.transform.translation.y = -gravity_zone[0]/1000
+        # t.transform.translation.z = gravity_zone[1]/1000
+        # if not np.isnan(gravity_zone[0]):
+        t.transform.translation.x = gravity_zone[0]/1000
+        t.transform.translation.y = gravity_zone[1]/1000
+        t.transform.translation.z = gravity_zone[2]/1000
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 1.0
+        tfm = tf2_msgs.msg.TFMessage([t]+tf_kp_list)
+        pub_tf.publish(tfm)
+        # save gravity
+        gravity_zone=gravity_zone.tolist()
+        gravity_zone.insert(0,float(img_time_str))
+        gravity_zone.append(float(odm_time_str))
+        gravity_zone.append(_odom_x)
+        gravity_zone.append(_odom_y)
+        gravity_zone.append(_odom_theta)
+        gravity_zone.append(pan)
+        gravity_history.append(gravity_zone)
+        np.savetxt(csv_path[:-4]+"_kp.csv",keypoints_history,delimiter=",")
+        np.savetxt(csv_path,gravity_history,delimiter=",")
+        # else:
+        #     print("!!!!!!!!!!!!!!!!!!!!!nan!!!!!!!!!!!!!!!!!!!!!")
     # rospy.loginfo("####### debug ROI #######")
     # rospy.loginfo(rgb_array.shape)
     # rospy.loginfo(dpt_array.shape)
