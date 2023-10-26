cc7a2648f97f4343489070529cc6905085a16555
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-10-26 17:55:18 +0900

    add data 2022b

--- sotsuron_experiment/scripts/test.py
+++ sotsuron_experiment/scripts/test.py
@@ -6,7 +6,7 @@ import numpy as np
 # import time
 # while True:
 csvpath="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/scripts/test.csv"
-csvpath="/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/ras_csv/03_00_00__2023-10-20-17-09-06_tf.csv"
+csvpath="/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/ras_csv/10_00_00__2023-10-22-14-37-09_tf.csv"
 data=pd.read_csv(csvpath)#,names=["tH","x","y","z","0","tR","xR","yR","thR","panR"])
 data=data.to_numpy()
 sort_idx=np.argsort(data,axis=0)[:,0]
