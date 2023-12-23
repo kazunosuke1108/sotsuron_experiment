f1c0d9b5acecc943653b8353e7a011d3fd98c201
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-12-23 11:40:01 +0900

    add data

--- sotsuron_experiment/scripts/test.py
+++ sotsuron_experiment/scripts/test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-data=pd.read_csv("C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/hayashide_robot_03_all.csv",header=None)
+data=pd.read_csv("C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/mahi12.csv",header=None)
 data=data.iloc[:3]
 
 for column in data.columns:
@@ -10,4 +10,4 @@ print(data)
 
 data_out=data.iloc[2]
 print(data_out)
-data_out.to_csv("C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/prefix.csv")
+data_out.to_csv("C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/prefix_mahi12.csv")
