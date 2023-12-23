ba7386076243b27dcd99ceefec8728cbb8981a58
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-12-23 15:51:28 +0900

    add all data

--- sotsuron_experiment/scripts/analysis_ras_vicon.py
+++ sotsuron_experiment/scripts/analysis_ras_vicon.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+from glob import glob
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from analysis_management import *
@@ -10,28 +11,34 @@ plt.rcParams["figure.autolayout"] = True
 plt.rcParams['font.family'] = 'Times New Roman'
 
 class analysisVicon():
-    def __init__(self):
+    def __init__(self,viconcsvpath="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/alldata/hayashide_robot_03_all.csv"):
         path_management,csv_labels,color_dict=management_initial()
         relationship_data=pd.read_csv("C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/vicon_history_relationship.csv")
         
         # VICON data
         # Look
-        self.viconcsvpath="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/hayashide_robot_03_all.csv"
+        self.viconcsvpath=viconcsvpath
         # self.viconcsvpath="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/hayashide_robot_05_modified.csv"
         # humpback
         # self.viconcsvpath="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/hayashide_robot_10_modified.csv"
         self.vicon_data=pd.read_csv(self.viconcsvpath,index_col=0,header=0)
-        self.vicon_data["hayashide_CentreOfMass_X"]=self.vicon_data["hayashide_CentreOfMass_X"]/1000
-        self.vicon_data["hayashide_CentreOfMass_Y"]=self.vicon_data["hayashide_CentreOfMass_Y"]/1000
-        self.vicon_data["hayashide_CentreOfMass_Z"]=self.vicon_data["hayashide_CentreOfMass_Z"]/1000
         self.vicon_data["robot_robot1_X"]=self.vicon_data["robot_robot1_X"]/1000
         self.vicon_data["robot_robot1_Y"]=self.vicon_data["robot_robot1_Y"]/1000
         self.vicon_data["robot_robot1_Z"]=self.vicon_data["robot_robot1_Z"]/1000
-        self.vicon_data["robot_robot1_X"]=self.vicon_data["robot_robot1_X"]-self.vicon_data["robot_robot1_X"].iat[0]
-        self.vicon_data["robot_robot1_Y"]=self.vicon_data["robot_robot1_Y"]-self.vicon_data["robot_robot1_Y"].iat[0]
+        offset_x=self.vicon_data["robot_robot1_X"].iat[0]
+        offset_y=self.vicon_data["robot_robot1_X"].iat[1]
+        self.vicon_data["robot_robot1_X"]=self.vicon_data["robot_robot1_X"]-offset_x
+        self.vicon_data["robot_robot1_Y"]=self.vicon_data["robot_robot1_Y"]-offset_y
         self.vicon_data["timestamp"]=self.vicon_data.index/60
-
+        try:
+            self.vicon_data["hayashide_CentreOfMass_X"]=self.vicon_data["hayashide_CentreOfMass_X"]/1000-offset_x
+            self.vicon_data["hayashide_CentreOfMass_Y"]=self.vicon_data["hayashide_CentreOfMass_Y"]/1000
+            self.vicon_data["hayashide_CentreOfMass_Z"]=self.vicon_data["hayashide_CentreOfMass_Z"]/1000
+        except KeyError:
+            pass
+        
         tfodomdir_path=relationship_data['sotsuron_experiment'].loc[relationship_data['vicon']==os.path.basename(self.viconcsvpath)[:-8]].values[0]
+        
         print(tfodomdir_path)
         # raise TimeoutError
 
@@ -147,6 +154,7 @@ class analysisVicon():
 
         gs = GridSpec(2, 2, width_ratios=[1,1])
         plt.subplot(gs[0,:])    
+        plt.title(os.path.basename(self.tfcsvpath)[:-11])   
         plt.plot(self.vicon_data["robot_robot1_X"],self.vicon_data["robot_robot1_Y"],"o-",markersize=3,label="robot (VICON)")
         plt.plot(self.odom_data["x"],self.odom_data["y"],"o-",markersize=3,label="robot (odometry)")
         plt.legend()
@@ -165,27 +173,35 @@ class analysisVicon():
         plt.legend()
         plt.xlabel("Time $\it{t}$ [s]")
         plt.ylabel("Hallway direction $\it{y}$ [m]")
-        plt.show()
+        # plt.show()
 
     def plot_gravity(self):
         gs = GridSpec(2, 2, width_ratios=[1,1])
         plt.subplot(gs[0,:])    
-        plt.plot(self.vicon_data["hayashide_CentreOfMass_X"],self.vicon_data["hayashide_CentreOfMass_Y"],"o-",markersize=3,label="gravity")
+        plt.title(os.path.basename(self.tfcsvpath)[:-11])   
+        self.tf_data["base_x"]=(self.tf_data["r_base_x"]+self.tf_data["l_base_x"])/2
+        self.tf_data["base_y"]=(self.tf_data["r_base_y"]+self.tf_data["l_base_y"])/2
+        plt.plot(self.vicon_data["hayashide_CentreOfMass_X"],self.vicon_data["hayashide_CentreOfMass_Y"],"o-",markersize=3,label="gravity (VICON)")
+        plt.plot(self.tf_data["base_x"],self.tf_data["base_y"],"o-",markersize=3,label="trunk")
         plt.legend()
         plt.xlabel("Hallway direction $/it{x}$ [m]")
         plt.ylabel("Width direction $/it{y}$ [m]")
         plt.gca().set_aspect('equal', adjustable='box')
         plt.subplot(gs[1,0])    
-        plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_CentreOfMass_X"],"o-",markersize=3,label="gravity_x")
+        plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_CentreOfMass_X"],"o-",markersize=3,label="gravity_x (VICON)")
+        plt.plot(self.tf_data["timestamp"],self.tf_data["base_x"],"o-",markersize=3,label="gravity")
         plt.legend()
         plt.xlabel("Time $/it{t}$ [s]")
         plt.ylabel("Hallway direction $/it{x}$ [m]")
         plt.subplot(gs[1,1])    
-        plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_CentreOfMass_Y"],"o-",markersize=3,label="gravity_y")
+        plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_CentreOfMass_Y"],"o-",markersize=3,label="gravity_y (VICON)")
+        plt.plot(self.tf_data["timestamp"],self.tf_data["base_y"],"o-",markersize=3,label="gravity")
         plt.legend()
         plt.xlabel("Time $\it{t}$ [s]")
         plt.ylabel("Hallway direction $\it{y}$ [m]")
-        plt.show()
+        plt.savefig(self.savedirpath+"/"+os.path.basename(self.tfcsvpath)[:-11]+"_gravity")
+        plt.cla()
+        # plt.show()
 
     def plot_head_angle(self):
         """
@@ -214,7 +230,8 @@ class analysisVicon():
                 self.tf_data["eye_head_angle_z"].iat[idx]=self.tf_data["eye_head_angle_z"].iat[idx]+180
 
         gs = GridSpec(3, 1)
-        plt.subplot(gs[0])    
+        plt.subplot(gs[0]) 
+        plt.title(os.path.basename(self.tfcsvpath)[:-11])   
         plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_LHeadAngles_X"],"o-",markersize=3,label="hayashide_LHeadAngles_X")
         # plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_RHeadAngles_X"],"o-",markersize=3,label="hayashide_RHeadAngles_X")
         plt.plot(self.tf_data["timestamp"],self.tf_data["eye_head_angle_x"],"o-",markersize=3,label="eye_head_angle_x")
@@ -241,6 +258,7 @@ class analysisVicon():
         plt.legend()
         plt.grid()
         plt.savefig(self.savedirpath+"/"+os.path.basename(self.tfcsvpath)[:-11]+"_head_angle")
+        plt.cla()
         # plt.show()
         # plt.cla()
         
@@ -281,7 +299,8 @@ class analysisVicon():
         self.tf_data["b_trunk_angle_z"]=np.rad2deg(np.arctan((self.tf_data["r_base_x"]-self.tf_data["l_base_x"])/(self.tf_data["r_base_y"]-self.tf_data["l_base_y"])))
 
         gs = GridSpec(3, 1)
-        plt.subplot(gs[0])    
+        plt.subplot(gs[0]) 
+        plt.title(os.path.basename(self.tfcsvpath)[:-11])   
         plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_LThoraxAngles_X"],"o-",markersize=3,label="hayashide_LThoraxAngles_X")
         plt.plot(self.tf_data["timestamp"],self.tf_data["r_trunk_angle_x"],"o-",markersize=3,label="r_trunk_angle_x")
         # plt.plot(self.tf_data["timestamp"],self.tf_data["l_trunk_angle_x"],"o-",markersize=3,label="l_trunk_angle_x")
@@ -308,13 +327,14 @@ class analysisVicon():
         plt.legend()
         plt.grid()
         plt.savefig(self.savedirpath+"/"+os.path.basename(self.tfcsvpath)[:-11]+"_trunk_angle")
+        plt.cla()
         # plt.cla()
-        plt.show()
+        # plt.show()
 
         # self.write_log([os.path.basename(self.tfcsvpath),self.tf_data["c_trunk_angle_y"].mean(),self.tf_data["c_trunk_angle_y"].median(),self.tf_data["c_trunk_angle_y"].std(),self.tf_data["c_trunk_angle_y"].min(),self.tf_data["c_trunk_angle_y"].max()],path_management["humpback_csv_path"],fmt="%s")
         pass
 
-    def plot_knee_angle(self):
+    def plot_hip_angle(self):
         """
         股関節の前後進展角度を求める
         """
@@ -331,36 +351,53 @@ class analysisVicon():
         self.tf_data["l_hip_angle_y"]=np.rad2deg(np.arctan((self.tf_data["l_knee_x"]-self.tf_data["l_base_x"])/(self.tf_data["l_knee_z"]-self.tf_data["l_base_z"])))
         self.tf_data["l_hip_angle_z"]=np.rad2deg(np.arctan((self.tf_data["l_knee_y"]-self.tf_data["l_base_y"])/(self.tf_data["l_knee_x"]-self.tf_data["l_base_x"])))
 
-        gs = GridSpec(3, 1)
-        plt.subplot(gs[0])    
-        plt.plot(self.tf_data["timestamp"],self.tf_data["r_hip_angle_x"],"o-",markersize=3,label="r_hip_angle_x")
-        plt.plot(self.tf_data["timestamp"],self.tf_data["l_hip_angle_x"],"o-",markersize=3,label="l_hip_angle_x")
+        gs = GridSpec(3, 1) 
+        plt.subplot(gs[0]) 
+        plt.title(os.path.basename(self.tfcsvpath)[:-11])   
+        plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_RHipAngles_X"],"o-",markersize=3,label="hayashide_RHipAngles_X")
+        # plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_LHipAngles_X"],"o-",markersize=3,label="hayashide_LHipAngles_X")
+        plt.plot(self.tf_data["timestamp"],self.tf_data["r_hip_angle_y"],"o-",markersize=3,label="r_hip_angle_y")
+        # plt.plot(self.tf_data["timestamp"],self.tf_data["l_hip_angle_x"],"o-",markersize=3,label="l_hip_angle_x")
         plt.xlabel("Time $/it{t}$ [s]")
         plt.ylabel("Hip angle in (hallway width direction) th_x [deg]")
         plt.legend()
         plt.grid()
         plt.subplot(gs[1])   
-        plt.plot(self.tf_data["timestamp"],self.tf_data["r_hip_angle_y"],"o-",markersize=3,label="r_hip_angle_y")
-        plt.plot(self.tf_data["timestamp"],self.tf_data["l_hip_angle_y"],"o-",markersize=3,label="l_hip_angle_y")
+        plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_RHipAngles_Y"],"o-",markersize=3,label="hayashide_RHipAngles_Y")
+        # # plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_LHipAngles_Y"],"o-",markersize=3,label="hayashide_LHipAngles_Y")
+        # # plt.plot(self.tf_data["timestamp"],self.tf_data["l_hip_angle_y"],"o-",markersize=3,label="l_hip_angle_y")
+        plt.plot(self.tf_data["timestamp"],self.tf_data["r_hip_angle_z"],"o-",markersize=3,label="r_hip_angle_z")
         plt.xlabel("Time $/it{t}$ [s]")
         plt.ylabel("Hip angle in (hallway direction) th_y [deg]")
         plt.legend()
         plt.grid()
         plt.subplot(gs[2])   
-        plt.plot(self.tf_data["timestamp"],self.tf_data["r_hip_angle_z"],"o-",markersize=3,label="r_hip_angle_z")
-        plt.plot(self.tf_data["timestamp"],self.tf_data["l_hip_angle_z"],"o-",markersize=3,label="l_hip_angle_z")
+        plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_RHipAngles_Z"],"o-",markersize=3,label="hayashide_RHipAnglesZ")
+        # plt.plot(self.vicon_data["timestamp"],self.vicon_data["hayashide_LHipAngles_Z"],"o-",markersize=3,label="hayashide_LHipAnglesZ")
+        plt.plot(self.tf_data["timestamp"],self.tf_data["r_hip_angle_x"],"o-",markersize=3,label="r_hip_angle_x")
+        # # plt.plot(self.tf_data["timestamp"],self.tf_data["l_hip_angle_z"],"o-",markersize=3,label="l_hip_angle_z")
         plt.xlabel("Time $/it{t}$ [s]")
         plt.ylabel("Hip angle in (yaw direction) th_z [deg]")
         plt.legend()
         plt.grid()
-        plt.savefig(self.savedirpath+"/"+os.path.basename(self.tfcsvpath)[:-11]+"_knee_angle.png")
+        plt.savefig(self.savedirpath+"/"+os.path.basename(self.tfcsvpath)[:-11]+"_hip_angle.png")
         plt.cla()
         pass
 
     def main(self):
-        self.plot_odom()
+        # self.plot_odom()
+        # self.plot_gravity()
         # self.plot_head_angle()
         self.plot_trunk_angle()
+        # self.plot_hip_angle()
 
-analysisv=analysisVicon()
-analysisv.main()
+
+viconcsvpaths=sorted(glob("C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20231220_VICON/alldata/*.csv"))
+for viconcsvpath in viconcsvpaths:
+    print(viconcsvpath)
+    try:
+        analysisv=analysisVicon(viconcsvpath)
+        analysisv.main()
+    except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(f"line {exc_tb.tb_lineno}: {e}")
