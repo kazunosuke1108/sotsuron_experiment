ee74a1c5358cffd190e8c837b45dbae02efb594f
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2024-01-29 17:25:08 +0900

    wip

--- sotsuron_experiment/scripts/analysis_vel_error_actualData.py
+++ sotsuron_experiment/scripts/analysis_vel_error_actualData.py
@@ -1,15 +1,21 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (15,10)
+plt.rcParams["figure.autolayout"] = True
+plt.rcParams['font.family'] = 'Times New Roman'
 
-odom_to_zed_csv_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/analysis/velocity_error/_2023-12-21-10-22-39_odom_to_zed.csv"
-zed_to_hmn_csv_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/analysis/velocity_error/_2023-12-21-10-22-39_zed_to_hmn.csv"
-odom_to_hmn_csv_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/analysis/velocity_error/_2023-12-21-10-22-39_odom_to_hmn.csv"
+odom_to_zed_csv_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/analysis/velocity_error/_2023-12-21-10-22-39_odom_to_zed_01x.csv"
+zed_to_hmn_csv_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/analysis/velocity_error/_2023-12-21-10-22-39_zed_to_hmn_01x.csv"
+odom_to_hmn_csv_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/analysis/velocity_error/_2023-12-21-10-22-39_odom_to_hmn_01x.csv"
 
 zR=pd.read_csv(odom_to_zed_csv_path,names=["t","x","y","z"])
 zHR=pd.read_csv(zed_to_hmn_csv_path,names=["t","x","y","z"])
 zH=pd.read_csv(odom_to_hmn_csv_path,names=["t","x","y","z"])
-plt.plot(zHR["t"],np.sqrt(zHR["y"]**2+zHR["z"]**2),"o-")
-plt.plot(zR["t"],zR["x"],"o-")
-plt.plot(zH["t"],zH["x"],"o-")
-plt.show()
+plt.plot(zHR["t"],np.sqrt(zHR["y"]**2+zHR["z"]**2),"o-",label="distance H&R")
+plt.plot(zR["t"],zR["x"],"o-",label="xR")
+plt.plot(zH["t"],zH["x"],"o-",label="xH")
+plt.xlabel("Time $\it{t}$ [s]")
+plt.ylabel("Position $\it{x}$ [m]")
+plt.legend()
+plt.savefig("C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/analysis/velocity_error/_2023-12-21-10-22-39_odom_to_zed_to_hmn_01x.png",dpi=500)
