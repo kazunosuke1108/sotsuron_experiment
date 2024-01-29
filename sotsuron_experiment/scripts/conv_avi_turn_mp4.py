26a276ce5a82ab6211b5e67380c5db349b0f8750
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2024-01-29 17:15:24 +0900

    wip

--- sotsuron_experiment/scripts/conv_avi_turn_mp4.py
+++ sotsuron_experiment/scripts/conv_avi_turn_mp4.py
@@ -8,29 +8,30 @@ from glob import glob
 try:
     avi_path=sys.argv[1]
 except Exception:
-    avi_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/results/20230605/movie/"
+    avi_path="/home/hayashide/catkin_ws/src/sotsuron_experiment/analysis/velocity_error/_2023-12-21-10-22-39.mp4"
 videos=[avi_path]
 
 for videoPath in videos:
     video_basename=os.path.basename(videoPath)
     print("now processing:",video_basename)
-    if video_basename[-4:]==".avi":
-        cap=cv2.VideoCapture(videoPath)
-        ret,frame=cap.read()
-        fourcc = cv2.VideoWriter_fourcc('m','p','4', 'v')
-        print(frame)
-        size=(frame.shape[1],frame.shape[0])
-        video=cv2.VideoWriter(avi_path+video_basename[:-4]+".mp4",fourcc, 30.0,size)
-        if cap.isOpened():
-            while True:
-                if ret:
-                    # frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-                    video.write(frame)
-                    print("added a frame at:",time.time(),"frame shape:",(frame.shape[0],frame.shape[1]))
-                else:
-                    break
-                ret,frame=cap.read()
-        video.release()
-        del video
-        del cap
-        del fourcc
+    # if video_basename[-4:]==".avi":
+    cap=cv2.VideoCapture(videoPath)
+    ret,frame=cap.read()
+    fourcc = cv2.VideoWriter_fourcc('m','p','4', 'v')
+    print(frame)
+    size=(frame.shape[0],frame.shape[1])
+    # size=(frame.shape[1],frame.shape[0])
+    video=cv2.VideoWriter(videoPath[:-4]+"_turn.mp4",fourcc, 30.0,size)
+    if cap.isOpened():
+        while True:
+            if ret:
+                frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                video.write(frame)
+                print("added a frame at:",time.time(),"frame shape:",(frame.shape[0],frame.shape[1]))
+            else:
+                break
+            ret,frame=cap.read()
+    video.release()
+    del video
+    del cap
+    del fourcc
