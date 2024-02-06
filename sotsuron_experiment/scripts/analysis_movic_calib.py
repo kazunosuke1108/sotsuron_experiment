ac0455b5e5e028cfa567aabc81f4daa3383ba5f1
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2024-02-07 00:18:58 +0900

    ~fft in trial 04

---
+++ sotsuron_experiment/scripts/analysis_movic_calib.py
@@ -0,0 +1,220 @@
+import numpy as np
+import pandas as pd
+from glob import glob
+import matplotlib.pyplot as plt
+
+from analysis_management import *
+from noise_processor import *
+
+class MovicExporter():
+    def __init__(self,results_dir_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20240204_04"):
+        self.path_management,self.csv_labels,self.color_dict=management_initial()
+        plt.rcParams["figure.figsize"] = (15,10)
+        plt.rcParams["figure.autolayout"] = True
+        plt.rcParams['font.family'] = 'Times New Roman'
+
+        self.results_dir_path=results_dir_path
+        self.base_accurate_imu_csv_path=self.results_dir_path+"/hsrb/base_accurate_imu_1.csv"
+        self.base_imu_csv_path=self.results_dir_path+"/hsrb/base_imu_1.csv"
+        self.head_imu_csv_path=self.results_dir_path+"/hsrb/imu_1.csv"
+        self.head_imu_calib_csv_path=self.head_imu_csv_path[:-5]+"calib_1.csv"
+        self.zed_imu_csv_path=self.results_dir_path+"/zed/imu_1.csv"
+        self.zed_imu_calib_csv_path=self.zed_imu_csv_path[:-5]+"calib_1.csv"
+        self.odom_csv_path=self.results_dir_path+"/hsrb/odom_1.csv"
+        self.command_velocity_csv_path=self.results_dir_path+"/hsrb/command_velocity_1.csv"
+        self.head_imu_calib_csv_path=self.results_dir_path+"/hsrb/imu_1_calib.csv"
+        self.zed_imu_calib_csv_path=self.results_dir_path+"/zed/imu_1_calib.csv"
+        self.odom_calib_csv_path=self.results_dir_path+"/hsrb/odom_1_calib.csv"
+        self.command_velocity_calib_csv_path=self.results_dir_path+"/hsrb/command_velocity_1_calib.csv"
+        self.exp_memo_csv_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/memo/20240204_exp_memo.csv"
+
+        self.trial_id=self.results_dir_path[-2:]
+
+        self.load_data()
+
+    def load_data(self):
+
+        try:
+            self.base_accurate_imu_data=pd.read_csv(self.base_accurate_imu_csv_path,names=self.csv_labels["imu"])
+        except FileNotFoundError:
+            pass
+        try:
+            self.base_imu_data=pd.read_csv(self.base_imu_csv_path,names=self.csv_labels["imu"])
+        except FileNotFoundError:
+            pass
+        try:
+            self.head_imu_data=pd.read_csv(self.head_imu_csv_path,names=self.csv_labels["imu"])
+        except FileNotFoundError:
+            pass
+        try:
+            self.zed_imu_data=pd.read_csv(self.zed_imu_csv_path,names=self.csv_labels["imu"])
+        except FileNotFoundError:
+            pass
+        try:
+            self.zed_imu_data=self.zed_imu_data[self.zed_imu_data["timestamp"]!=self.zed_imu_data["timestamp"].min()] # 362行目にzeroが入ってたので消去
+        except FileNotFoundError:
+            pass
+        try:
+            self.head_imu_calib_data=pd.read_csv(self.head_imu_calib_csv_path,names=self.csv_labels["imu"])
+        except FileNotFoundError:
+            pass
+        try:
+            self.zed_imu_calib_data=pd.read_csv(self.zed_imu_calib_csv_path,names=self.csv_labels["imu"])
+        except FileNotFoundError:
+            pass
+        try:
+            self.zed_imu_data=self.zed_imu_data[self.zed_imu_data["timestamp"]!=self.zed_imu_data["timestamp"].min()] # 362行目にzeroが入ってたので消去
+        except FileNotFoundError:
+            pass
+        try:
+            self.odom_data=pd.read_csv(self.odom_csv_path,names=self.csv_labels["odometry"])
+        except FileNotFoundError:
+            pass
+        try:
+            self.command_velocity_data=pd.read_csv(self.command_velocity_csv_path,names=self.csv_labels["command_velocity"])
+        except FileNotFoundError:
+            pass
+
+        self.exp_memo_data=pd.read_csv(self.exp_memo_csv_path,header=0)
+        print(self.exp_memo_data)
+
+    def calib_data(self,data,data_name):
+        calib_start=self.exp_memo_data[self.exp_memo_data["trial_id"]==float(self.trial_id)]["calib_start"].values[0]
+        calib_end=calib_start+self.exp_memo_data[self.exp_memo_data["trial_id"]==float(self.trial_id)]["calib_duration"].values[0]
+        print(calib_end)
+        calib_data=data[data["timestamp"]>=calib_start]
+        calib_data=data[data["timestamp"]<=calib_end]
+        stable_acc_x=calib_data["lin_acc_x"].mean()
+        stable_acc_y=calib_data["lin_acc_y"].mean()
+        stable_acc_z=calib_data["lin_acc_z"].mean()
+        if data_name=="head_imu_data":
+            g=np.sqrt(stable_acc_x**2+stable_acc_z**2)
+            stable_angle=-np.arccos(stable_acc_z/g)
+            self.head_imu_data["lin_acc_x_calib"]=self.head_imu_data["lin_acc_x"]*np.cos(stable_angle)-self.head_imu_data["lin_acc_z"]*np.sin(stable_angle)
+            self.head_imu_data["lin_acc_y_calib"]=self.head_imu_data["lin_acc_y"]
+            self.head_imu_data["lin_acc_z_calib"]=self.head_imu_data["lin_acc_x"]*np.sin(stable_angle)+self.head_imu_data["lin_acc_z"]*np.cos(stable_angle)
+            # plt.plot(self.head_imu_data["timestamp"],self.head_imu_data["lin_acc_x_calib"],"r")
+            # plt.plot(self.head_imu_data["timestamp"],self.head_imu_data["lin_acc_y_calib"],"g")
+            # plt.plot(self.head_imu_data["timestamp"],self.head_imu_data["lin_acc_z_calib"],"b")
+            # print(stable_acc_z)
+            # print(stable_angle)
+            # print(np.median(np.sqrt(self.head_imu_data["lin_acc_x_calib"].values**2+self.head_imu_data["lin_acc_z_calib"].values**2)))
+            self.head_imu_data.to_csv(self.head_imu_calib_csv_path,index=False)
+            raise TimeoutError
+        if data_name=="zed_imu_data":
+            g=np.sqrt(stable_acc_x**2+stable_acc_y**2)
+            stable_angle=np.arccos(abs(stable_acc_y)/g)
+            self.zed_imu_data["lin_acc_y_calib"]=self.zed_imu_data["lin_acc_y"]*np.cos(stable_angle)-self.zed_imu_data["lin_acc_x"]*np.sin(stable_angle)
+            self.zed_imu_data["lin_acc_x_calib"]=self.zed_imu_data["lin_acc_y"]*np.sin(stable_angle)+self.zed_imu_data["lin_acc_x"]*np.cos(stable_angle)
+            self.zed_imu_data["lin_acc_z_calib"]=self.zed_imu_data["lin_acc_z"]
+            plt.plot(self.zed_imu_data["timestamp"],self.zed_imu_data["lin_acc_x_calib"],"r")
+            plt.plot(self.zed_imu_data["timestamp"],self.zed_imu_data["lin_acc_y_calib"],"g")
+            plt.plot(self.zed_imu_data["timestamp"],self.zed_imu_data["lin_acc_z_calib"],"b")
+            print(g)
+            print(stable_angle)
+            print(np.median(np.sqrt(self.zed_imu_data["lin_acc_x_calib"].values**2+self.zed_imu_data["lin_acc_y_calib"].values**2)))
+            self.zed_imu_data.to_csv(self.zed_imu_calib_csv_path,index=False)
+
+            raise TimeoutError
+
+
+    def plot_imu(self,data,data_name):
+        plt.plot(data["timestamp"],data["lin_acc_x"],"r",label="lin_acc_x(accurate)")
+        # plt.plot(base_imu_data["timestamp"],base_imu_data["lin_acc_x"],"",label="lin_acc_x")
+        plt.plot(data["timestamp"],data["lin_acc_y"],"g",label="lin_acc_y(accurate)")
+        plt.plot(data["timestamp"],data["lin_acc_z"],"b",label="lin_acc_z(accurate)")
+        plt.legend()
+        plt.grid()
+        plt.xlabel("Time $\it{t}$ [s]")
+        plt.ylabel("Acceleration $\it{a}$ [m/s$^{2}$]")
+        plt.title(data_name+" "+os.path.basename(self.results_dir_path))
+        plt.savefig(self.results_dir_path+f"/{data_name}.png")
+        plt.cla()
+
+
+
+    def plot_odom(self,data,data_name):
+        # print(np.mean(data["timestamp"].values[1:]-data["timestamp"].values[:-1]))
+        # print(np.std(data["timestamp"].values[1:]-data["timestamp"].values[:-1]))
+        # print(np.min(data["timestamp"].values[1:]-data["timestamp"].values[:-1]))
+        # print(np.max(data["timestamp"].values[1:]-data["timestamp"].values[:-1]))
+        # plt.hist(data["timestamp"].values[1:]-data["timestamp"].values[:-1])
+        plt.cla()
+        # raise TimeoutError
+        # data=resampling_processor(data)
+        data=vel_processor(data)
+        fig,ax1=plt.subplots()
+        ax1.plot(data["timestamp"],data["x"],"r",label="$\it{x}$")
+        ax1.plot(data["timestamp"],data["y"],"g",label="$\it{y}$")
+        ax1.plot(data["timestamp"],data["theta"],"b",label=r"$\theta$")
+        ax1.plot(data["timestamp"],data["pan"],"c",label="$\phi$")
+        ax1.legend(loc="upper left")
+        ax1.grid()
+        ax1.set_xlabel("Time $\it{t}$ [s]")
+        ax1.set_ylabel("Position [m]")
+        ax2=ax1.twinx()
+        ax2.plot(data["timestamp"],data["v_x"],"r--",label="$\it{v_x}$ [m/s]")
+        ax2.plot(data["timestamp"],data["v_y"],"g--",label="$\it{v_y}$ [m/s]")
+        ax2.plot(data["timestamp"],data["v_theta"],"b--",label=r"$v_{\theta}$ [rad/s]")
+        ax2.plot(data["timestamp"],data["v_pan"],"c--",label="$v_{\phi}$ [rad/s]")
+        ax2.legend(loc="upper right")
+        ax2.set_ylabel("Velocity [m/s]")
+
+        plt.title(data_name+" "+os.path.basename(self.results_dir_path))
+        plt.savefig(self.results_dir_path+f"/{data_name}.png")
+        plt.cla()
+
+    def plot_command_velocity(self,data,data_name):
+        plt.plot(data["timestamp"],data["v_x"],"r",label="$\it{v_x}$ [m/s]")
+        plt.plot(data["timestamp"],data["v_y"],"g",label="$\it{v_y}$ [m/s]")
+        plt.plot(data["timestamp"],data["omg_z"],"b",label=r"$v_{\theta}$+v_{\phi} [rad/s]")
+        plt.legend()
+        plt.grid()
+        plt.xlabel("Time $\it{t}$ [s]")
+        plt.ylabel("Velocity")
+        plt.title(data_name+" "+os.path.basename(self.results_dir_path))
+        plt.savefig(self.results_dir_path+f"/{data_name}.png")
+        plt.cla()
+
+
+    def plot_all(self):
+        ## IMU
+        try:
+            self.plot_imu(self.base_accurate_imu_data,"base_accurate_imu_data")
+        except AttributeError:
+            pass
+        try:
+            self.plot_imu(self.base_imu_data,"base_imu_data")
+        except AttributeError:
+            pass
+        try:
+            self.plot_imu(self.head_imu_data,"head_imu_data")
+        except AttributeError:
+            pass
+        try:
+            self.plot_imu(self.zed_imu_data,"zed_imu_data")
+        except AttributeError:
+            pass
+
+        ## Odometry
+        try:
+            self.plot_odom(self.odom_data,"odom_data")
+        except AttributeError:
+            pass
+
+        ## Command velocity
+        try:
+            self.plot_command_velocity(self.command_velocity_data,"command_velocity_data")
+        except AttributeError:
+            pass
+
+    def main(self):
+        self.calib_data(self.zed_imu_data,"zed_imu_data")
+
+results_dirs=sorted(glob("C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20240204_*"))
+# print(results_dirs)
+cls=MovicExporter(results_dirs[3])
+cls.main()
+# for results_dir_path in results_dirs:
+    # cls=MovicExporter(results_dir_path)
+    # cls.main()
--- sotsuron_experiment/scripts/analysis_movic_sudden_stop.py
+++ sotsuron_experiment/scripts/analysis_movic_sudden_stop.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from analysis_management import *
 from noise_processor import *
+from matplotlib.gridspec import GridSpec
 
 class AnalyzeSuddenStop():
     def __init__(self,results_dir_path="C:/Users/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/results/20240204_04"):
@@ -17,10 +18,12 @@ class AnalyzeSuddenStop():
         self.results_dir_path=results_dir_path
         self.base_accurate_imu_csv_path=self.results_dir_path+"/hsrb/base_accurate_imu_1.csv"
         self.base_imu_csv_path=self.results_dir_path+"/hsrb/base_imu_1.csv"
-        self.head_imu_csv_path=self.results_dir_path+"/hsrb/imu_1.csv"
-        self.zed_imu_csv_path=self.results_dir_path+"/zed/imu_1.csv"
+        self.head_imu_csv_path=self.results_dir_path+"/hsrb/imu_1_calib.csv"
+        self.zed_imu_csv_path=self.results_dir_path+"/zed/imu_1_calib.csv"
         self.odom_csv_path=self.results_dir_path+"/hsrb/odom_1.csv"
         self.command_velocity_csv_path=self.results_dir_path+"/hsrb/command_velocity_1.csv"
+
+        self.load_data()
 
     def load_data(self):
         try:
@@ -42,43 +45,154 @@ class AnalyzeSuddenStop():
         except FileNotFoundError:
             pass
         try:
-            self.head_imu_data=pd.read_csv(self.head_imu_csv_path,names=self.csv_labels["imu"])
+            self.head_imu_data=pd.read_csv(self.head_imu_csv_path,header=0)
+            print(self.head_imu_data["timestamp"])
             self.head_imu_data=self.head_imu_data[self.head_imu_data["timestamp"]>=start_timestamp]
             self.head_imu_data=self.head_imu_data[self.head_imu_data["timestamp"]<=end_timestamp]
         except FileNotFoundError:
             pass
         try:
-            self.zed_imu_data=pd.read_csv(self.zed_imu_csv_path,names=self.csv_labels["imu"])
+            self.zed_imu_data=pd.read_csv(self.zed_imu_csv_path,header=0)
             self.zed_imu_data=self.zed_imu_data[self.zed_imu_data["timestamp"]>=start_timestamp]
             self.zed_imu_data=self.zed_imu_data[self.zed_imu_data["timestamp"]<=end_timestamp]
-        except FileNotFoundError:
-            pass
-        try:
             self.zed_imu_data=self.zed_imu_data[self.zed_imu_data["timestamp"]!=self.zed_imu_data["timestamp"].min()] # 362行目にzeroが入ってたので消去
         except FileNotFoundError:
             pass
         try:
             self.odom_data=pd.read_csv(self.odom_csv_path,names=self.csv_labels["odometry"])
+            self.odom_data=vel_processor(self.odom_data)
             self.odom_data=self.odom_data[self.odom_data["timestamp"]>=start_timestamp]
             self.odom_data=self.odom_data[self.odom_data["timestamp"]<=end_timestamp]
         except FileNotFoundError:
             pass
 
+    def plot_all_imu(self):
+        # fig,ax1=plt.subplots()
+        gs = GridSpec(2, 1)
+        plt.subplot(gs[0])
+        plt.plot(self.base_accurate_imu_data["timestamp"],self.base_accurate_imu_data["lin_acc_x"],label=r"base $\it{\alpha_x}$")
+        plt.plot(self.head_imu_data["timestamp"],-self.head_imu_data["lin_acc_x_calib"],label=r"head $\it{\alpha_x}$")
+        plt.plot(self.zed_imu_data["timestamp"],self.zed_imu_data["lin_acc_x_calib"],label=r"zed $\it{\alpha_x}$")
+        plt.legend(loc="upper left")
+        plt.grid()
+        plt.xlabel("Time $\it{t}$ [s]")
+        plt.ylabel("Acceleration $\it{a}$ [m/s$^{2}$]")
+        plt.subplot(gs[1])
+        plt.plot(self.command_velocity_data["timestamp"],self.command_velocity_data["v_x"],label="command")
+        plt.plot(self.odom_data["timestamp"],self.odom_data["v_x"],label="odometry")
+        plt.grid()
+        plt.legend(loc="upper right")
+        plt.xlabel("Time $\it{t}$ [s]")
+        plt.ylabel("Velocity $\it{v_x}$ [m/s]")
+        plt.title("all IMU and velocity"+" "+os.path.basename(self.results_dir_path))
+        plt.savefig(self.results_dir_path+"/all_IMU_and_vel.png")
+        plt.clf()
 
+    def plot_fft(self,fft_start_timestamp=None,fft_end_timestamp=None,png_suffix="accelOff_and_2s"):
+        if fft_start_timestamp==None:
+            fft_start_timestamp=self.command_velocity_data["timestamp"].max()
+        if fft_end_timestamp==None:
+            fft_end_timestamp=self.command_velocity_data["timestamp"].max()+2
+
+        brake_odom_data=self.odom_data[self.odom_data["timestamp"]>=fft_start_timestamp]
+        brake_odom_data=brake_odom_data[self.odom_data["timestamp"]<=fft_end_timestamp]
+        brake_base_accurate_imu_data=self.base_accurate_imu_data[self.base_accurate_imu_data["timestamp"]>=fft_start_timestamp]
+        brake_base_accurate_imu_data=brake_base_accurate_imu_data[self.base_accurate_imu_data["timestamp"]<=fft_end_timestamp]
+        brake_head_imu_data=self.head_imu_data[self.head_imu_data["timestamp"]>=fft_start_timestamp]
+        brake_head_imu_data=brake_head_imu_data[self.head_imu_data["timestamp"]<=fft_end_timestamp]
+        brake_zed_imu_data=self.zed_imu_data[self.zed_imu_data["timestamp"]>=fft_start_timestamp]
+        brake_zed_imu_data=brake_zed_imu_data[self.zed_imu_data["timestamp"]<=fft_end_timestamp]
+
+        def plot_before_fft():
+            gs = GridSpec(2, 1)
+            plt.subplot(gs[0])
+            plt.plot(brake_base_accurate_imu_data["timestamp"],brake_base_accurate_imu_data["lin_acc_x"],label=r"base $\it{\alpha_x}$")
+            plt.plot(brake_head_imu_data["timestamp"],-brake_head_imu_data["lin_acc_x_calib"],label=r"head $\it{\alpha_x}$")
+            plt.plot(brake_zed_imu_data["timestamp"],brake_zed_imu_data["lin_acc_x_calib"],label=r"zed $\it{\alpha_x}$")
+            plt.legend(loc="upper left")
+            plt.grid()
+            plt.xlabel("Time $\it{t}$ [s]")
+            plt.ylabel("Acceleration $\it{a}$ [m/s$^{2}$]")
+            plt.subplot(gs[1])
+            # plt.plot(self.command_velocity_data["timestamp"],self.command_velocity_data["v_x"],label="command")
+            plt.plot(brake_odom_data["timestamp"],brake_odom_data["v_x"],label="odometry")
+            plt.grid()
+            plt.legend(loc="upper right")
+            plt.xlabel("Time $\it{t}$ [s]")
+            plt.ylabel("Velocity $\it{v_x}$ [m/s]")
+            plt.title("all IMU and velocity"+" "+os.path.basename(self.results_dir_path))
+            plt.savefig(self.results_dir_path+f"/all_IMU_and_vel_before_fft_{png_suffix}.png")
+            plt.clf()
+
+        plot_before_fft()
+
+        def calc_fft(data):
+            ## resample
+            data=resampling_processor(data)
+            ## definition
+            print(data)
+            N=len(data)
+            dt=0.01
+            fs=1/dt # サンプリング周波数（標本点の間隔^-1）
+            fn=fs/2 # ナイキスト周波数（再現可能な周波数の上限値）
+            freq=np.fft.rfftfreq(N,d=dt)
+
+            # from scipy import signal
+            # window = signal.hann(N)  # ハニング窓関数(開始・終了地点にずれが生じてしまう場合の解消法．トレンド除去済みのデータになら適用できるかも)
+            try:
+                F_vx=np.fft.rfft(data["lin_acc_x_calib"],axis=0)
+                F_vy=np.fft.rfft(data["lin_acc_y_calib"],axis=0)
+                F_vz=np.fft.rfft(data["lin_acc_z_calib"],axis=0)
+            except KeyError:
+                F_vx=np.fft.rfft(data["lin_acc_x"],axis=0)
+                F_vy=np.fft.rfft(data["lin_acc_y"],axis=0)
+                F_vz=np.fft.rfft(data["lin_acc_z"],axis=0)
+
+            F_vx=F_vx/(N/2)
+            F_vy=F_vy/(N/2)
+            F_vz=F_vz/(N/2)
+
+            Amp_x=np.abs(F_vx)
+            Amp_y=np.abs(F_vy)
+            Amp_z=np.abs(F_vy)
+
+            return freq[:N//2], Amp_x[:N//2]
+        
+        [freq_base_accurate_imu_data,amp_base_accurate_imu_data]=calc_fft(brake_base_accurate_imu_data)
+        [freq_head_imu_data,amp_head_imu_data]=calc_fft(brake_head_imu_data)
+        [freq_zed_imu_data,amp_zed_imu_data]=calc_fft(brake_zed_imu_data)
+        # gs = GridSpec(3, 1)
+        # plt.subplot(gs[0])
+        plt.clf()
+        plt.plot(freq_base_accurate_imu_data,amp_base_accurate_imu_data,label="base")
+        plt.plot(freq_head_imu_data,amp_head_imu_data,label="head")
+        plt.plot(freq_zed_imu_data,amp_zed_imu_data,label="zed")
+        plt.legend()
+        plt.grid()
+        plt.xlabel("Frequency [Hz]")
+        plt.xlabel("Amplitude")
+        plt.title("FFT of the main IMUs when stop"+" "+os.path.basename(self.results_dir_path))
+        plt.savefig(self.results_dir_path+f"/all_IMU_fft_{png_suffix}.png")
+        # plt.show()
+        pass
         
 
     def plot_imu(self,data,data_name):
-        plt.plot(data["timestamp"],data["lin_acc_x"],"r",label="lin_acc_x(accurate)")
-        # plt.plot(base_imu_data["timestamp"],base_imu_data["lin_acc_x"],"",label="lin_acc_x")
-        plt.plot(data["timestamp"],data["lin_acc_y"],"g",label="lin_acc_y(accurate)")
-        plt.plot(data["timestamp"],data["lin_acc_z"],"b",label="lin_acc_z(accurate)")
+        try:
+            plt.plot(data["timestamp"],data["lin_acc_x_calib"],"r",label="lin_acc_x (calib)")
+            plt.plot(data["timestamp"],data["lin_acc_y_calib"],"g",label="lin_acc_y (calib)")
+            plt.plot(data["timestamp"],data["lin_acc_z_calib"],"b",label="lin_acc_z (calib)")
+        except KeyError:
+            plt.plot(data["timestamp"],data["lin_acc_x"],"r",label="lin_acc_x")
+            plt.plot(data["timestamp"],data["lin_acc_y"],"g",label="lin_acc_y")
+            plt.plot(data["timestamp"],data["lin_acc_z"],"b",label="lin_acc_z")
         plt.legend()
         plt.grid()
         plt.xlabel("Time $\it{t}$ [s]")
         plt.ylabel("Acceleration $\it{a}$ [m/s$^{2}$]")
         plt.title(data_name+" "+os.path.basename(self.results_dir_path))
         plt.savefig(self.results_dir_path+f"/{data_name}_{self.png_prefix}.png")
-        plt.cla()
+        plt.clf()
 
     def plot_odom(self,data,data_name):
         # print(np.mean(data["timestamp"].values[1:]-data["timestamp"].values[:-1]))
@@ -86,7 +200,7 @@ class AnalyzeSuddenStop():
         # print(np.min(data["timestamp"].values[1:]-data["timestamp"].values[:-1]))
         # print(np.max(data["timestamp"].values[1:]-data["timestamp"].values[:-1]))
         # plt.hist(data["timestamp"].values[1:]-data["timestamp"].values[:-1])
-        plt.cla()
+        plt.clf()
         # raise TimeoutError
         # data=resampling_processor(data)
         data=vel_processor(data)
@@ -109,7 +223,7 @@ class AnalyzeSuddenStop():
 
         plt.title(data_name+" "+os.path.basename(self.results_dir_path))
         plt.savefig(self.results_dir_path+f"/{data_name}_{self.png_prefix}.png")
-        plt.cla()
+        plt.clf()
 
     def plot_command_velocity(self,data,data_name):
         plt.plot(data["timestamp"],data["v_x"],"r",label="$\it{v_x}$ [m/s]")
@@ -121,7 +235,7 @@ class AnalyzeSuddenStop():
         plt.ylabel("Velocity")
         plt.title(data_name+" "+os.path.basename(self.results_dir_path))
         plt.savefig(self.results_dir_path+f"/{data_name}_{self.png_prefix}.png")
-        plt.cla()
+        plt.clf()
 
     def plot_all(self):
         self.load_data()
@@ -156,4 +270,13 @@ class AnalyzeSuddenStop():
             pass
 
 cls=AnalyzeSuddenStop()
-cls.plot_all()
+cls.plot_all_imu()
+cls.plot_fft()
+# start_timestamp=cls.command_velocity_data["timestamp"].min()
+# end_timestamp=start_timestamp+1
+# idx=0
+# while end_timestamp<=cls.command_velocity_data["timestamp"].max()+2:
+#     cls.plot_fft(start_timestamp,end_timestamp,str(start_timestamp))
+#     start_timestamp=start_timestamp+0.5
+#     end_timestamp=start_timestamp+1
+#     print(start_timestamp)
